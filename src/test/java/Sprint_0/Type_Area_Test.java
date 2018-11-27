cc9314e1d22828299feff52462294eabfc4e40e0
teresavarela <teresavarelagomes@gmail.com>
2018-11-27 11:09:53 +0000

    Classe 'TypeArea' - simplificação dos métodos, alteração nome variáveis Classe 'Type_Area_Test' - Testes Set

--- src/test/java/Sprint_0/Type_Area_Test.java
+++ src/test/java/Sprint_0/Type_Area_Test.java
@@ -5,15 +5,44 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class Type_Area_Test {
+
     @Test
-    public void seeIfSetGetTypeWorks(){
+    public void checkTypeOfGeographicAreaConstructor(){
         //Arrange
         String expectedResult = "Rua";
         String actualResult;
-        TypeArea c = new TypeArea("Rua");
+        //Act
+        TypeArea typeArea1 = new TypeArea("Rua");
+        actualResult = typeArea1.getTypeOfGeographicArea();
+
+        //Assert
+        assertEquals(expectedResult,actualResult);
+    }
+
+    @Test
+    public void checkSetGeographicAreaTypeWithSameDesignationFromConstructor(){
+        //Arrange
+        String expectedResult = "Rua";
+        String actualResult;
+        TypeArea typeArea1 = new TypeArea("Rua");
 
         //Act
-        actualResult = c.getType();
+        typeArea1.setTypeOfGeographicArea("Rua");
+        actualResult = typeArea1.getTypeOfGeographicArea();
+
+        //Assert
+        assertEquals(expectedResult,actualResult);
+    }
+    @Test
+    public void checkSetGeographicAreaTypeWithDifferentDesignationFromConstructor(){
+        //Arrange
+        String expectedResult = "Freguesia";
+        String actualResult;
+        TypeArea typeArea1 = new TypeArea("Rua");
+
+        //Act
+        typeArea1.setTypeOfGeographicArea("Freguesia");
+        actualResult = typeArea1.getTypeOfGeographicArea();
 
         //Assert
         assertEquals(expectedResult,actualResult);
