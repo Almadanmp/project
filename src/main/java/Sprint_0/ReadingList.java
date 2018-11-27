6af545ae4a59f1ab701bb0ac0d63870626f26df1
migue <1181718@isep.ipp.pt>
2018-11-27 10:58:32 +0000

    Class ReadingList - Added Descriptions

--- src/main/java/Sprint_0/ReadingList.java
+++ src/main/java/Sprint_0/ReadingList.java
@@ -3,13 +3,24 @@ package Sprint_0;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * This is the ReadingList Class, A List of Readings that the Sensor gets.
+ */
 public class ReadingList {
     List<Reading> mReadings;
 
+    /**
+     * Empty Constructor to always create an ArrayList of Readings.
+     */
     public ReadingList() {
         mReadings = new ArrayList<>();
     }
 
+    /**
+     * Method to Add a reading only if it's not contained in the list already.
+     * @param reading
+     * @return
+     */
     public boolean addReading (Reading reading) {
         if (!(mReadings.contains(reading))) {
             mReadings.add(reading);
@@ -19,17 +30,30 @@ public class ReadingList {
         }
     }
 
+    /**
+     *
+     * @param reading
+     * @return
+     */
     public boolean containsReading (Reading reading) {
         return mReadings.contains(reading);
     }
 
+    /**
+     * Setter
+     * @param readings
+     */
     public void setReadings(List<Reading> readings) {
         this.mReadings = readings;
     }
 
+    /**
+     * Getter
+     * @return
+     */
     public List<Reading> getListOfReadings () {
         return this.mReadings;
     }
 }
 
-}
+
