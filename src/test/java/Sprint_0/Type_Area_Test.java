75da6e9f70d92df356a24e9a1af8c43d2b90a2e3
teresavarela <teresavarelagomes@gmail.com>
2018-11-27 14:43:48 +0000

    Type Area Test - tests

--- src/test/java/Sprint_0/Type_Area_Test.java
+++ src/test/java/Sprint_0/Type_Area_Test.java
@@ -47,4 +47,67 @@ public class Type_Area_Test {
         //Assert
         assertEquals(expectedResult,actualResult);
     }
+
+    @Test
+    public void checkGetGeographicAreaTypeAfterSet(){
+        //Arrange
+        String expectedResult = "Porto";
+        String actualResult;
+        TypeArea typeArea1 = new TypeArea("Rua");
+
+        //Act
+        typeArea1.setTypeOfGeographicArea("Porto");
+        actualResult = typeArea1.getTypeOfGeographicArea();
+
+        //Assert
+        assertEquals(expectedResult,actualResult);
+    }
+
+    @Test
+    public void checkEqualsBetweenTwoGeographicAreaTypesWithSameDesignation(){
+        //Arrange
+        Boolean expectedResult = true;
+        Boolean actualResult;
+        TypeArea typeArea1 = new TypeArea("Rua");
+        TypeArea typeArea2 = new TypeArea("Rua");
+        //Act
+        actualResult = typeArea1.equals(typeArea2);
+        //Assert
+        assertEquals(expectedResult,actualResult);
+    }
+    @Test
+    public void checkEqualsBetweenTwoGeographicAreaTypesWithDifferentDesignation(){
+        //Arrange
+        Boolean expectedResult = false;
+        Boolean actualResult;
+        TypeArea typeArea1 = new TypeArea("Rua");
+        TypeArea typeArea2 = new TypeArea("Freguesia");
+        //Act
+        actualResult = typeArea1.equals(typeArea2);
+        //Assert
+        assertEquals(expectedResult,actualResult);
+    }
+    @Test
+    public void checkEqualsBetweenSameObject(){
+        //Arrange
+        Boolean expectedResult = true;
+        Boolean actualResult;
+        TypeArea typeArea1 = new TypeArea("Rua");
+        //Act
+        actualResult = typeArea1.equals(typeArea1);
+        //Assert
+        assertEquals(expectedResult,actualResult);
+    }
+    @Test
+    public void checkEqualsBetweenObjectsFromDifferentClasses(){
+        //Arrange
+        Boolean expectedResult = false;
+        Boolean actualResult;
+        TypeArea typeArea1 = new TypeArea("Rua");
+        int number = 1;
+        //Act
+        actualResult = typeArea1.equals(number);
+        //Assert
+        assertEquals(expectedResult,actualResult);
+    }
 }
