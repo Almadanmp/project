7a2237f5b54848c6594f988c4f34b6717470b8bb
JCachada <jmcs.cachada@gmail.com>
2018-11-27 21:58:25 +0000

    Local class at 100% coverage.

--- src/test/java/Sprint_0/Local_Test.java
+++ src/test/java/Sprint_0/Local_Test.java
@@ -10,7 +10,7 @@ public class Local_Test {
         //Arrange
         double expectedResult = 21;
         double actualResult;
-        Local c = new Local(21,3);
+        Local c = new Local(21, 3);
 
         //Act
         actualResult = c.getLatitude();
@@ -24,7 +24,7 @@ public class Local_Test {
         //Arrange
         double expectedResult = 3;
         double actualResult;
-        Local c = new Local(21,3);
+        Local c = new Local(21, 3);
 
         //Act
         actualResult = c.getLongitude();
@@ -38,7 +38,7 @@ public class Local_Test {
         //Arrange
         double expectedResult = 57;
         double actualResult;
-        Local c = new Local(21,3, 210);
+        Local c = new Local(21, 3, 210);
 
         //Act
         c.setAltitude(57);
@@ -49,7 +49,7 @@ public class Local_Test {
     }
 
     @Test
-    public void seeIfGetLinearDistanceBetweenAreasWorks(){
+    public void seeIfGetLinearDistanceBetweenAreasWorks() {
         //Arrange
         Local l1 = new Local(41.16, -8.63);
         Local c = new Local(38.74, -9.14);
@@ -57,9 +57,60 @@ public class Local_Test {
         double expectedResult = 2.473;
 
         //Act
-        actualResult = c.getLinearDistanceBetweenLocals(l1,c);
+        actualResult = c.getLinearDistanceBetweenLocals(l1, c);
 
-        assertEquals(expectedResult,actualResult, 0.001);
+        assertEquals(expectedResult, actualResult, 0.001);
     }
 
+    @Test
+    public void seeIfEqualsWorksSameObjects() {
+        //Arrange
+        Local l1 = new Local(23, 42, 2);
+        Local l2 = l1;
+        boolean expectedResult = true;
+        boolean actualResult;
+
+        //Act
+        actualResult = l1.equals(l2);
+
+        //Assert
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void seeIfEqualsWorksNotInstance() {
+        //Arrange
+        Local l1 = new Local(23, 42, 2);
+        TypeSensor t1 = new TypeSensor("Rua");
+        boolean expectedResult = false;
+        boolean actualResult;
+
+        //Act
+        actualResult = l1.equals(t1);
+
+        //Assert
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void seeIfEqualsWorksDifferentObject(){
+        //Arrange
+        Local l1 = new Local(23, 42, 2);
+        Local l2 = new Local(21,21,5);
+        boolean expectedResult = false;
+        boolean actualResult;
+
+        //Act
+        actualResult = l1.equals(l2);
+
+        //Assert
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void hashCodeDummyTest(){
+        int expectedResult = 22671767;
+        int actualResult = hashCode();
+        assertEquals(expectedResult,actualResult);
+    }
 }
