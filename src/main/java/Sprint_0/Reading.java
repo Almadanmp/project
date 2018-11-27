5e9301e300256871a512e47cec9f4fd57eab6443
Cárina <carina.alas11@gmail.com>
2018-11-27 10:39:23 +0000

    Added Class 'Reading' and Test 'ReadingTest'

---
+++ src/main/java/Sprint_0/Reading.java
@@ -0,0 +1,50 @@
+package Sprint_0;
+
+import java.util.Date;
+
+public class Reading {
+    private double mValue;
+    private Date mDate;
+
+    public Reading(double value, Date date) {
+        setmValue(value);
+        setData(date);
+    }
+
+    public Reading(double value) {
+        setmValue(value);
+    }
+
+    public void setmValue(double value) {
+        this.mValue = value;
+    }
+
+    public double getmValue() {
+        return this.mValue;
+    }
+
+    public void setData(Date date) {
+        this.mDate = date;
+    }
+
+    public Date getmDate() {
+        return this.mDate;
+    }
+
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof Reading)) {
+            return false;
+        } else {
+            Reading reading = (Reading) o;
+            if (this.mValue == reading.getmValue() && this.mDate.equals(reading.getmDate())) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+
+}
