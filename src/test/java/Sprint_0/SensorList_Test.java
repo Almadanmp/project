dca4fa1f3eb72254c2dccbad7d576fd7ca7ca12e
JCachada <jmcs.cachada@gmail.com>
2018-11-28 10:38:57 +0000

    SensorList: Added "getMostRecentlyUsedSensor"

--- src/test/java/Sprint_0/SensorList_Test.java
+++ src/test/java/Sprint_0/SensorList_Test.java
@@ -9,7 +9,6 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 public class SensorList_Test {
