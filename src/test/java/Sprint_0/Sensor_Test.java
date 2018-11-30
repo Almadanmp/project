9af92ccc1f691ff851e4dbba9f2924f70b9ca4fc
1181716 <1181716@isep.ipp.pt>
2018-11-30 10:51:45 +0000

    Sensor_Test - more tests

--- src/test/java/Sprint_0/Sensor_Test.java
+++ src/test/java/Sprint_0/Sensor_Test.java
@@ -15,7 +15,8 @@ public class Sensor_Test {
     @Test
     public void seeIfSetGetNameWorks() {
         //Arrange
-        Sensor c = new Sensor("Vento", new TypeSensor("Atmosphere"), new Local(12, 31, 21), new Date());
+        Sensor c = new Sensor("Vento", new TypeSensor("Atmosphere"),
+                new Local(12, 31, 21), new Date());
         c.setName("Temperatura");
         String expectedResult = "Temperatura";
         String actualResult;
@@ -28,9 +29,42 @@ public class Sensor_Test {
     }
 
     @Test
+    public void seeIfSetGetNameWorksSecondName() {
+        //Arrange
+        Sensor c = new Sensor("Vento", new TypeSensor("Atmosphere"),
+                new Local(12, 31, 21), new Date());
+        c.setName("Temperatura");
+        c.setName("Chuva");
+        String expectedResult = "Chuva";
+        String actualResult;
+
+        //Act
+        actualResult = c.getName();
+
+        //Assert
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void seeIfSetGetNameWorksNoSet() {
+        //Arrange
+        Sensor c = new Sensor("Vento", new TypeSensor("Atmosphere"),
+                new Local(12, 31, 21), new Date());
+        String expectedResult = "Vento";
+        String actualResult;
+
+        //Act
+        actualResult = c.getName();
+
+        //Assert
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
     public void seeIfSetGetLocalWorks() {
         //Arrange
-        Sensor c = new Sensor("Vento", new TypeSensor("Atmosphere"), new Local(12, 31, 21), new Date());
+        Sensor c = new Sensor("Vento", new TypeSensor("Atmosphere"),
+                new Local(12, 31, 21), new Date());
         Local testLocal = new Local(34, 2, 110);
         Local expectedResult = new Local(34, 2, 110);
         Local actualResult;
@@ -44,9 +78,44 @@ public class Sensor_Test {
     }
 
     @Test
+    public void seeIfSetGetLocalWorksSecondLocal() {
+        //Arrange
+        Sensor c = new Sensor("Vento", new TypeSensor("Atmosphere"),
+                new Local(12, 31, 21), new Date());
+        Local testLocal1 = new Local(34, 2, 110);
+        Local testLocal2 = new Local(30, 20, 110);
+        Local expectedResult = new Local(30, 20, 110);
+        Local actualResult;
+
+        //Act
+        c.setLocal(testLocal1);
+        c.setLocal(testLocal2);
+        actualResult = c.getLocal();
+
+        //Assert
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void seeIfSetGetLocalWorksNoSet() {
+        //Arrange
+        Sensor c = new Sensor("Vento", new TypeSensor("Atmosphere"),
+                new Local(12, 31, 21), new Date());
+        Local expectedResult = new Local(12, 31, 21);
+        Local actualResult;
+
+        //Act
+        actualResult = c.getLocal();
+
+        //Assert
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
     public void seeIfGetSetTypeSensorWorks() {
         //Arrange
-        Sensor c = new Sensor("Vento", new TypeSensor("Atmosphere"), new Local(12, 31, 21), new Date());
+        Sensor c = new Sensor("Vento", new TypeSensor("Atmosphere"),
+                new Local(12, 31, 21), new Date());
         TypeSensor testSensor = new TypeSensor("Atmosphere");
         TypeSensor expectedResult = new TypeSensor("Atmosphere");
         TypeSensor actualResult;
@@ -62,7 +131,8 @@ public class Sensor_Test {
     @Test
     public void seeIfGetSetDateStartedFunctioningWorks() {
         //Arrange
-        Sensor c = new Sensor("Vento", new TypeSensor("Atmosphere"), new Local(12, 31, 21), new Date());
+        Sensor c = new Sensor("Vento", new TypeSensor("Atmosphere"),
+                new Local(12, 31, 21), new Date());
         Calendar myCalendar = new GregorianCalendar(2014, Calendar.FEBRUARY, 11);
         Date expectedResult = new GregorianCalendar(2014, Calendar.FEBRUARY, 11).getTime();
         Date actualResult;
@@ -78,7 +148,8 @@ public class Sensor_Test {
     @Test
     public void seeIfSeTAndGetReadingList() {
         //Arrange
-        Sensor c = new Sensor("Vento", new TypeSensor("Atmosphere"), new Local(12, 31, 21), new Date());
+        Sensor c = new Sensor("Vento", new TypeSensor("Atmosphere"),
+                new Local(12, 31, 21), new Date());
         ReadingList rl1 = new ReadingList();
         Reading reading1 = new Reading(15, new GregorianCalendar(118, 11, 25).getTime());
         rl1.addReading(reading1);
@@ -93,9 +164,10 @@ public class Sensor_Test {
     }
 
     @Test
-    public void seeIfEqualsWorksNotAnInstance() {
+    public void seeIfEqualsWorksNotAnInstanceFalse() {
         //Arrange
-        Sensor s1 = new Sensor("Vento", new TypeSensor("Atmosphere"), new Local(12, 31, 21), new Date());
+        Sensor s1 = new Sensor("Vento", new TypeSensor("Atmosphere"),
+                new Local(12, 31, 21), new Date());
         Local l1 = new Local(21, 23, 1);
         boolean expectedResult = false;
         boolean actualResult;
@@ -107,10 +179,78 @@ public class Sensor_Test {
         assertEquals(expectedResult, actualResult);
     }
 
+    @Test
+    public void seeIfEqualsWorksFalseDifferentSensor() {
+        //Arrange
+        Sensor s1 = new Sensor("Vento", new TypeSensor("Atmosphere"),
+                new Local(12, 31, 21), new Date());
+        Sensor s2 = new Sensor("Chuva", new TypeSensor("Chuva"),
+                new Local(10, 30, 20), new Date());
+        boolean expectedResult = false;
+        boolean actualResult;
+
+        //Act
+        actualResult = s1.equals(s2);
+
+        //Assert
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void seeIfEqualsWorksFalseDifferentName() {
+        //Arrange
+        Sensor s1 = new Sensor("Vento", new TypeSensor("Atmosphere"),
+                new Local(12, 31, 21), new Date());
+        Sensor s2 = new Sensor("Chuva", new TypeSensor("Atmosphere"),
+                new Local(12, 31, 21), new Date());
+        boolean expectedResult = false;
+        boolean actualResult;
+
+        //Act
+        actualResult = s1.equals(s2);
+
+        //Assert
+        assertEquals(expectedResult, actualResult);
+    }
+
+
+    @Test
+    public void seeIfEqualsWorksTrueSameSensor() {
+        //Arrange
+        Sensor s1 = new Sensor("Vento", new TypeSensor("Atmosphere"),
+                new Local(12, 31, 21), new Date());
+        Sensor s2 = new Sensor("Vento", new TypeSensor("Atmosphere"),
+                new Local(12, 31, 21), new Date());
+        boolean expectedResult = true;
+        boolean actualResult;
+
+        //Act
+        actualResult = s1.equals(s2);
+
+        //Assert
+        assertEquals(expectedResult, actualResult);
+    }
+    @Test
+    public void seeIfEqualsWorksTrueSameName() {
+        //Arrange
+        Sensor s1 = new Sensor("Vento", new TypeSensor("Atmosphere"),
+                new Local(12, 31, 21), new Date());
+        Sensor s2 = new Sensor("Vento", new TypeSensor("Chuva"),
+                new Local(10, 30, 20), new Date());
+        boolean expectedResult = true;
+        boolean actualResult;
+
+        //Act
+        actualResult = s1.equals(s2);
+
+        //Assert
+        assertEquals(expectedResult, actualResult);
+    }
 
     @Test
     public void hashCodeDummyTest() {
-        Sensor s1 = new Sensor("Vento", new TypeSensor("Atmosphere"), new Local(12, 31, 21), new Date());
+        Sensor s1 = new Sensor("Vento", new TypeSensor("Atmosphere"),
+                new Local(12, 31, 21), new Date());
         int expectedResult = 1;
         int actualResult = s1.hashCode();
         assertEquals(expectedResult, actualResult);
