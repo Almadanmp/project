33bf9eefbd011846e3a71d8e9ae51b7d0fd9955b
1181716 <1181716@isep.ipp.pt>
2018-11-30 10:17:21 +0000

    Sensor - javadoc comments

--- src/main/java/Sprint_0/Sensor.java
+++ src/main/java/Sprint_0/Sensor.java
@@ -14,6 +14,14 @@ public class Sensor {
     private Date dateStartedFunctioning;
     private ReadingList readingList;
 
+
+    /**
+     * Constructor with:name, typesensor, local, date
+     * @param name
+     * @param typeSensor
+     * @param local
+     * @param date
+     */
     public Sensor(String name, TypeSensor typeSensor, Local local, Date date) {
         setName(name);
         setTypeSensor(typeSensor);
@@ -21,6 +29,14 @@ public class Sensor {
         setDateStartedFunctioning(date);
     }
 
+    /**
+     *Constructor with name, type sensor, local, date, reading list
+     * @param name
+     * @param typeSensor
+     * @param local
+     * @param date
+     * @param readingList
+     */
     public Sensor(String name, TypeSensor typeSensor, Local local, Date date, ReadingList readingList) {
         setName(name);
         setTypeSensor(typeSensor);
@@ -30,7 +46,8 @@ public class Sensor {
     }
 
     /**
-     * Getters and Setters.
+     * Setter: name
+     * @param name
      */
     public void setName(String name) {
         if (AuxiliaryMethods.isNameValid(name)) {
@@ -38,46 +55,87 @@ public class Sensor {
         }
     }
 
+    /**
+     * Setter: local
+     * @param local
+     */
     public void setLocal(Local local) {
         this.local = local;
     }
 
+    /**
+     * Setter: type sensor
+     * @param sensor
+     */
     public void setTypeSensor(TypeSensor sensor) {
         this.typeSensor = sensor;
     }
 
+    /**
+     * Setter: date started functioning
+     * @param dateStartedFunctioning
+     */
     public void setDateStartedFunctioning(Date dateStartedFunctioning) {
         this.dateStartedFunctioning = dateStartedFunctioning;
     }
 
+    /**
+     * Getter: name
+     * @return
+     */
     public String getName() {
         String result = this.name;
         return result;
     }
 
+    /**
+     * Getter: type sensor
+     * @return
+     */
     public TypeSensor getTypeSensor() {
         TypeSensor result = this.typeSensor;
         return result;
     }
 
+    /**
+     * Getter: local
+     * @return
+     */
     public Local getLocal() {
         Local result = this.local;
         return result;
     }
 
+    /**
+     * Getter: date started functioning
+     * @return
+     */
     public Date getDateStartedFunctioning() {
         Date result = this.dateStartedFunctioning;
         return result;
     }
 
+    /**
+     * Getter: reading list
+     * @return
+     */
     public ReadingList getReadingList() {
         return readingList;
     }
 
+    /**
+     * Setter: reading list
+     * @param readingList
+     */
     public void setReadingList(ReadingList readingList) {
         this.readingList = readingList;
     }
 
+    /**
+     * Method to calculate distance to sensor from a sensor to a local
+     * @param s1 - sensor
+     * @return the distance from a local to sensor in km (doubles)
+     */
     public double calculateDistanceToSensor(Sensor s1) {
         Local l = s1.getLocal();
         return this.local.getLinearDistanceBetweenLocalsInKm(l);
