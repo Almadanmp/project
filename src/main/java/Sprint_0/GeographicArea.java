a4133ea41ac7d044030130929de9ba9578b220a9
JCachada <jmcs.cachada@gmail.com>
2018-12-05 11:34:07 +0000

    Half of first functionality implemented. Added vertices as attributes of Geographic Area, constructor for vertices and setters / getters, plus javadocs.

--- src/main/java/Sprint_0/GeographicArea.java
+++ src/main/java/Sprint_0/GeographicArea.java
@@ -5,16 +5,20 @@ package Sprint_0;
  */
 
 public class GeographicArea {
-    private TypeArea typeArea;
-    private Local local;
-    private SensorList sensorList;
+    private TypeArea mTypeArea;
+    private Local mLocal;
+    private SensorList mSensorList;
+    private Local mTopLeftVertex;
+    private Local mBottomRightVertex;
 
 
     // GeoArea constructors. The minimum amount of data for a GeoArea is a place and a type of area.
     // They can be made with or without a sensor list.
 
-    /** Constructor of the class GeographicArea that receives the type of Geographic Area and its localization
+    /**
+     * Constructor of the class GeographicArea that receives the type of Geographic Area and its localization
      * as parameters.
+     *
      * @param typeArea Type area is determined by a string - e.g. "Street", "City", etc.
      * @param local    Localization is defined by longitude, latitude and altitude.
      */
@@ -23,10 +27,12 @@ public class GeographicArea {
         setLocal(local);
     }
 
-    /** Constructor of the class GeographicArea that receives the type of Geographic Area, its localization
+    /**
+     * Constructor of the class GeographicArea that receives the type of Geographic Area, its location
      * and a list of sensors as parameters.
-     * @param typeArea Type area is determined by a string - e.g. "Street", "City", etc.
-     * @param local    Localization is defined by three doubles (longitude, latitude and altitude).
+     *
+     * @param typeArea   Type area is determined by a string - e.g. "Street", "City", etc.
+     * @param local      Localization is defined by three doubles (longitude, latitude and altitude).
      * @param sensorList Defined by a List<Sensor>.
      */
     public GeographicArea(TypeArea typeArea, Local local, SensorList sensorList) {
@@ -35,70 +41,130 @@ public class GeographicArea {
         setSensorList(sensorList);
     }
 
-    // Setters and Getters for all the parameters.
-    /** Setter for Geographic Area type.
-     * @param typeArea Type area is determined by a string - e.g. "Street", "City", etc.
+    /**
+     * Constructor of the class GeographicArea that receives the type of Geographic Area, its location, a
+     * list of sensors contained in it, and two vertices. The two vertices define a rectangular physical
+     * area that matches an approximation of the physical area covered by the Geographic Area.
      */
-    public void setTypeArea(TypeArea typeArea) {
-        this.typeArea = typeArea;
+
+    public GeographicArea(TypeArea typeArea, Local local, SensorList sensorList, Local topLeftVertex, Local bottomRightVertex) {
+        setTypeArea(typeArea);
+        setLocal(local);
+        setTopLeftVertex(topLeftVertex);
+        setBottomRightVertex(bottomRightVertex);
     }
 
-    /** Setter for Geographic Area localization.
-     * @param local Localization is defined by three doubles (longitude, latitude and altitude).
+    // Setters and Getters for all the parameters.
+
+    /**
+     * Setters for the Vertexes of the geographic area. For the purposes of the area they define, the area
+     * is assumed to be a rectangle.
      */
-    public void setLocal(Local local) {
-        this.local = local;
+
+    public void setTopLeftVertex(Local local) {
+        this.mTopLeftVertex = local;
+    }
+
+    public void setBottomRightVertex(Local local) {
+        this.mBottomRightVertex = local;
     }
 
     /**
+     * Setter for Geographic Area type.
+     *
+     * @param typeArea Type area is determined by a string - e.g. "Street", "City", etc.
+     */
+    public void setTypeArea(TypeArea typeArea) {
+        this.mTypeArea = typeArea;
+    }
+
+    /**
+     * Setter for Geographic Area localization.
+     *
+     * @param local Localization is defined by three doubles (longitude, latitude and altitude).
+     */
+    public void setLocal(Local local) {
+        this.mLocal = local;
+    }
+
+
+    /**
      * Setter for Sensor List.
+     *
      * @param listToSet
      */
+
     public void setSensorList(SensorList listToSet) {
-        this.sensorList = listToSet;
+        this.mSensorList = listToSet;
     }
-    /** Getter for type of Geographic Area.
-     * @return  returns the attribute TypeArea from an object of the class Geographic Area
+
+    /**
+     * @return returns a Local object containing the top left Vertex.
+     */
+    public Local getTopLeftVertex() {
+        return mTopLeftVertex;
+    }
+
+    /**
+     * @return returns a Local object containing the bottom right Vertex.
+     */
+
+    public Local getBottomRightVertex() {
+        return mBottomRightVertex;
+    }
+
+    /**
+     * Getter for type of Geographic Area.
+     *
+     * @return returns the attribute TypeArea from an object of the class Geographic Area
      */
     public TypeArea getTypeArea() {
-        return this.typeArea;
+        return this.mTypeArea;
     }
 
-    /** Getter for Geographic Area localization.
-     * @return  returns the attribute local from an object of the class Geographic Area
+    /**
+     * Getter for Geographic Area localization.
+     *
+     * @return returns the attribute local from an object of the class Geographic Area
      */
     public Local getLocal() {
-        return this.local;
+        return this.mLocal;
     }
 
-    /** Getter for Geographic Area sensor list.
-     * @return  returns the attribute sensorList from an object of the class Geographic Area
+    /**
+     * Getter for Geographic Area sensor list.
+     *
+     * @return returns the attribute sensorList from an object of the class Geographic Area
      */
     public SensorList getSensorList() {
-        return this.sensorList;
+        return this.mSensorList;
     }
 
-    /** Method will go through Geographic Area's sensor list, create a second list with the type
+    /**
+     * Method will go through Geographic Area's sensor list, create a second list with the type
      * of sensors defined by the parameter and finally return the most recent value recorded in that list.
+     *
      * @param typeOfSensor Type sensor is determined by a string - e.g. "Temperature", "Rain", etc.
-     * @return  returns a double of the most recent value recorded in every type sensor given
+     * @return returns a double of the most recent value recorded in every type sensor given
      */
-    public double getMostRecentReadingValue(String typeOfSensor){
-        SensorList listToTest = this.sensorList;
-        for(int i = 0; i < listToTest.getSensorList().size(); i++){
-            if(!(listToTest.getSensorList().get(i).getTypeSensor().getName().equals(typeOfSensor))){
+    public double getMostRecentReadingValue(String typeOfSensor) {
+        SensorList listToTest = this.mSensorList;
+        for (int i = 0; i < listToTest.getSensorList().size(); i++) {
+            if (!(listToTest.getSensorList().get(i).getTypeSensor().getName().equals(typeOfSensor))) {
                 listToTest.removeSensor(listToTest.getSensorList().get(i));
             }
         }
         return listToTest.getMostRecentlyUsedSensor().getReadingList().getMostRecentReading().getmValue();
     }
 
-    /** Method will calculate the distance between two different Geographic Areas.
-     * @param ga  object of the class GeographicArea
-     * @return  returns a double of the distance between Geographic Areas.
+    /**
+     * Method will calculate the distance between two different Geographic Areas.
+     *
+     * @param ga object of the class GeographicArea
+     * @return returns a double of the distance between Geographic Areas.
      */
-    public double calculateDistanceToGA(GeographicArea ga){
+    public double calculateDistanceToGA(GeographicArea ga) {
         Local l = ga.getLocal();
-        return this.local.getLinearDistanceBetweenLocalsInKm(l);
+        return this.mLocal.getLinearDistanceBetweenLocalsInKm(l);
     }
 }
