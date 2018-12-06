4273d58f82a505e77efb6c34fb6b8ea6abb72cc7
Andre Rua <1181689@isep.ipp.pt>
2018-12-06 13:16:20 +0000

    Testing UI

---
+++ src/main/java/Sprint_0/MainUI.java
@@ -0,0 +1,27 @@
+package Sprint_0;
+
+import java.util.Scanner;
+
+public class MainUI {
+
+    public static void main(String[] args)
+    {
+    Scanner input = new Scanner(System.in);
+
+    //Titulo consola
+
+    System.out.println("*************************************************\n"+
+                       "************** Menu Principal Teste *************\n"+
+                       "******************sWitCh 2018********************\n"+
+                       "*************************************************");
+
+    // Input da selecao do submenu
+    String[] menuArray = {
+            "1. Opcao1",
+            "2. Opcao2",
+            "3. Opcao3",
+            "4. Opcao4",
+            "5. Opcao5",
+    };
+    }
+}
