7d78f80abf0f0c1c4f7a6a31c736a2635c6a5e39
Andre Rua <1181689@isep.ipp.pt>
2018-12-07 16:50:00 +0000

    User Story 06 updates

--- src/main/java/Sprint0/Controller/US06Controller.java
+++ src/main/java/Sprint0/Controller/US06Controller.java
@@ -1,9 +1,6 @@
 package Sprint0.Controller;
 
-import Sprint0.Model.Local;
-import Sprint0.Model.ReadingList;
-import Sprint0.Model.Sensor;
-import Sprint0.Model.TypeSensor;
+import Sprint0.Model.*;
 
 import java.util.ArrayList;
 import java.util.Date;
@@ -13,23 +10,33 @@ import java.util.List;
 public class US06Controller {
 
     private Sensor mSensor;
-    private String mName;
-    private TypeSensor mTypeSensor;
-    private Local mLocal;
-    private Date mDateStartedFunctioning;
-    private ReadingList mReadingList;
+    private String mSensorName;
+    private TypeSensor mSensorTypeSensor;
+    private Local mSensorLocal;
+    private Date mSensorDateStartedFunctioning;
+    private ReadingList mSensorReadingList;
     private List<Sensor> mSensorList;
+    private GeographicArea GA;
 
     public US06Controller(Sensor sensor) {
         this.mSensor = sensor;
     }
 
-    boolean addSensor(Sensor sensorToAdd){
+    public boolean addSensor(Sensor sensorToAdd){
+        this.mSensor = sensorToAdd;
         mSensorList = new ArrayList<>();
         if (!(mSensorList.contains(sensorToAdd))) {
             mSensorList.add(sensorToAdd);
         }
         return false;
     }
+    public void addSensorToGeographicArea(Sensor sensorToAdd) {
+
+    }
+    public void setType(Sensor sensor,String name){
+        TypeSensor type = new TypeSensor();
+        type.setName(name);
+    }
 
 }
+
