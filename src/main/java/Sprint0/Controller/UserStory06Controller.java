5e85da12a17c6a0282365b0c2fde98926f02e742
Andre Rua <1181689@isep.ipp.pt>
2018-12-07 16:17:34 +0000

    User Story 06 updates

--- src/main/java/Sprint0/Controller/UserStory06Controller.java
+++ src/main/java/Sprint0/Controller/UserStory06Controller.java
@@ -2,16 +2,34 @@ package Sprint0.Controller;
 
 import Sprint0.Model.Local;
 import Sprint0.Model.ReadingList;
+import Sprint0.Model.Sensor;
 import Sprint0.Model.TypeSensor;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+
 
 public class UserStory06Controller {
+
+    private Sensor mSensor;
     private String mName;
     private TypeSensor mTypeSensor;
     private Local mLocal;
     private Date mDateStartedFunctioning;
     private ReadingList mReadingList;
+    private List<Sensor> mSensorList;
 
+    public UserStory06Controller(Sensor sensor) {
+        this.mSensor = sensor;
+    }
 
+    boolean addSensor(Sensor sensorToAdd){
+        mSensorList = new ArrayList<>();
+        if (!(mSensorList.contains(sensorToAdd))) {
+            mSensorList.add(sensorToAdd);
+        }
+        return false;
+    }
+    
 }
--- src/main/java/Sprint0/View/UserStory06View.java
+++ src/main/java/Sprint0/View/UserStory06View.java
@@ -1,20 +1,30 @@
 package Sprint0.View;
 
+import Sprint0.Controller.UserStory06Controller;
+import Sprint0.Model.Sensor;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class UserStory06View {
 
-    public static void main(String[] args) {
+    private List<String> mSensors;
+    private UserStory06Controller mController;
+
+    public UserStory06View(Sensor sensor) {
         Scanner input = new Scanner(System.in);
-
+        UserStory06Controller controller = new UserStory06Controller(sensor);
         //Console title
-
         System.out.println("***************************************************\n"+
                            "************** Sensor Addition Menu ***************\n"+
                            "****************** sWitCh 2018 ********************\n"+
                            "***************************************************\n");
 
         System.out.println("New Sensor Input:" );
-        
+        String sensorNome = input.nextLine();
+        System.out.println("You entered sensor " + sensorNome);
+        mSensors.add(sensorNome);
+        System.out.println(mSensors);
     }
 }
