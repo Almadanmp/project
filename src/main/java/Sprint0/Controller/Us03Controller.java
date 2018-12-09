468a81c3c3aa279cd4c7fbaf1663a54f04f33b1f
jcachada <1181711@isep.ipp.pt>
2018-12-09 16:39:06 +0000

    Attempt to fix compiler in Jenkins - locally, Maven is returning Build Success.

--- src/main/java/Sprint0/Controller/Us03Controller.java
+++ src/main/java/Sprint0/Controller/Us03Controller.java
@@ -1,14 +1,10 @@
 package Sprint0.Controller;
 
-import Sprint0.Model.GeographicArea;
+/**
+ * User Story 3
+ * As a System Administrator I want to Create a new Geographic Area
+ */
+
 
 public class US03Controller {
-
-    /**
-     * User Story 3
-     * As a System Administrator I want to Create a new Geographic Area
-     */
-    public US03Controller(GeographicArea GA1){
-
-    }
 }
