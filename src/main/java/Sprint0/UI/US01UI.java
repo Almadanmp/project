2cef6b4c28449f9041e2f8e4ea864599c010af1e
Cárina <carina.alas11@gmail.com>
2018-12-10 11:56:47 +0000

    New Class US01UI, US01Controller, TypeAreaList.

---
+++ src/main/java/Sprint0/UI/US01UI.java
@@ -0,0 +1,31 @@
+package Sprint0.UI;
+
+import Sprint0.Controller.US01Controller;
+import Sprint0.Model.TypeAreaList;
+
+import java.util.Scanner;
+
+public class US01UI {
+    private US01Controller ctrl;
+    private boolean active;
+
+    public US01UI(TypeAreaList type){
+        active=false;
+        ctrl= new US01Controller(type);
+    }
+
+    public void run(){
+        System.out.println("Please insert the name of the new Geographic Area Type");
+        while(this.active){
+        Scanner ler= new Scanner(System.in);
+        String nome = ler.nextLine();
+        if(ctrl.newTAG(nome)==true){
+            System.out.println("Success");
+        }
+        else{
+            System.out.println("Unsuccess");
+        }
+        }
+    }
+
+}
