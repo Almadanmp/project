2cc033608aaa94bcdb22503576ea21179249fe42
Dark <1181722@isep.ipp.pt>
2018-12-11 13:29:48 +0000

    US02Controller Classe criada

---
+++ src/main/java/Sprint0/Controller/US02Controller.java
@@ -0,0 +1,23 @@
+package Sprint0.Controller;
+
+import Sprint0.UI.MainUI;
+import Sprint0.Model.TypeAreaList;
+import Sprint0.Model.TypeArea;
+
+import java.util.List;
+
+
+/**
+ * User Story 2
+ *
+ * As a System Administrator I want to receive a list of all the previously stated Types of area.
+ */
+
+public class US02Controller {
+
+    private TypeAreaList mTypeAreaList;
+
+    public List<TypeArea> getTypeAreaList() {
+        return this.mTypeAreaList.getTypeAreaList();
+    }
+}
