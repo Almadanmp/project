c922c032e267bd67cc8a76bc9cb8e3ced1754ceb
migue <1181718@isep.ipp.pt>
2018-12-12 13:26:37 +0000

    ref #3 Class US04ControllerTest seeThatWeSetGeographicAreaListWithGeographicAreasFromTypeGiven()

--- src/test/java/Sprint0_Test/ControllerTest/US04ControllerTest.java
+++ src/test/java/Sprint0_Test/ControllerTest/US04ControllerTest.java
@@ -6,6 +6,7 @@ import Sprint0.Model.GeographicAreaList;
 import Sprint0.Model.Local;
 import Sprint0.Model.TypeArea;
 import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import static org.testng.Assert.*;
 
@@ -47,6 +48,41 @@ public class US04ControllerTest {
         assertEquals(expectedResult, actualResult);
     }
 
+    @Test
+    public void seeThatWeSetGeographicAreaListWithGeographicAreasFromTypeGiven() {
+
+        //Arrange
+        GeographicAreaList geographicAreaList = new GeographicAreaList();
+        TypeArea t1 = new TypeArea("Rua");
+        Local l1 = new Local(38, 7);
+        GeographicArea ga1 = new GeographicArea(t1, l1);
+        TypeArea t2 = new TypeArea("Cidade");
+        Local l2 = new Local(40, 7);
+        GeographicArea ga2 = new GeographicArea(t2, l2);
+        TypeArea t3 = new TypeArea("Rua");
+        Local l3 = new Local(38, 59);
+        GeographicArea ga3 = new GeographicArea(t3, l3);
+        TypeArea t4 = new TypeArea("Montanha");
+        Local l4 = new Local(38, 32);
+        GeographicArea ga4 = new GeographicArea(t4, l4);
+
+        geographicAreaList.addGeographicAreaToGeographicAreaList(ga1);
+        geographicAreaList.addGeographicAreaToGeographicAreaList(ga4);
+        geographicAreaList.addGeographicAreaToGeographicAreaList(ga2);
+        geographicAreaList.addGeographicAreaToGeographicAreaList(ga3);
+        US04Controller ctrl = new US04Controller(geographicAreaList);
+        GeographicAreaList expectedResult = new GeographicAreaList();
+        expectedResult.addGeographicAreaToGeographicAreaList(ga1);
+        expectedResult.addGeographicAreaToGeographicAreaList(ga3);
+
+        //Act
+        GeographicAreaList result;
+        ctrl.setGeographicAreaListWithGeographicAreasFromTypeGivenUS04UI("Rua");
+        result = ctrl.getGeographicAreaList();
+
+        //Assert
+        assertEquals(expectedResult, result);
+    }
 
 
 }
