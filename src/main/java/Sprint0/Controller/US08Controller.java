8ad47f49864a2aeb1a44d75cdda6f9795214b1f5
danie <1181699@isep.ipp.pt>
2018-12-13 16:20:17 +0000

    US08 - Tested.

--- src/main/java/Sprint0/Controller/US08Controller.java
+++ src/main/java/Sprint0/Controller/US08Controller.java
@@ -12,33 +12,45 @@ public class US08Controller {
     private GeographicAreaList mGeographicAreaList;
 
 
-    public US08Controller(GeographicAreaList list){this.mGeographicAreaList = list;}
+    public US08Controller(GeographicAreaList list) {
+        this.mGeographicAreaList = list;
+    }
+
+    public GeographicArea getmGeographicAreaContained() {
+        return this.mGeographicAreaContained;
+    }
+
+    public GeographicArea getmGeographicAreaContainer() {
+        return this.mGeographicAreaContainer;
+    }
 
     /**
      * This method define the GeographicAreas Container and Contained
+     *
      * @param nameOfAreaContained
      * @param nameOfAreaContainer
      * @return
      */
-    public boolean matchGeographicAreas(String nameOfAreaContained, String nameOfAreaContainer){
+    public boolean matchGeographicAreas(String nameOfAreaContained, String nameOfAreaContainer) {
         if (checkIfListIsValid(mGeographicAreaList.getGeographicAreaList())) {
             for (GeographicArea ga1 : mGeographicAreaList.getGeographicAreaList()) {
                 if (ga1.getName().equals(nameOfAreaContained)) {
                     mGeographicAreaContained = ga1;
-                    break;
-                }
-            }
-            for (GeographicArea ga2 : mGeographicAreaList.getGeographicAreaList()) {
+                    for (GeographicArea ga2 : mGeographicAreaList.getGeographicAreaList()) {
                         if (ga2.getName().equals(nameOfAreaContainer)) {
                             mGeographicAreaContainer = ga2;
                             return true;
                         }
                     }
-        }return false;
+                }
+            }
+        }
+        return false;
     }
 
     /**
      * This methods checks if one area (AreaContained) is contained in another area (AreaContainer)
+     *
      * @return
      */
     public boolean seeIfItsContained() {
@@ -48,12 +60,14 @@ public class US08Controller {
 
     /**
      * This method checks if the list exists
+     *
      * @param values
      * @return
      */
-    private boolean checkIfListIsValid(List<GeographicArea> values){
-        if (values == null || values.isEmpty()){
+    private boolean checkIfListIsValid(List<GeographicArea> values) {
+        if (values == null || values.isEmpty()) {
             return false;
-        }return true;
+        }
+        return true;
     }
 }
--- src/test/java/Sprint0_Test/ControllerTest/US08ControllerTest.java
+++ src/test/java/Sprint0_Test/ControllerTest/US08ControllerTest.java
@@ -1,5 +1,117 @@
 package Sprint0_Test.ControllerTest;
 
+import Sprint0.Controller.US08Controller;
+import Sprint0.Model.GeographicArea;
+import Sprint0.Model.GeographicAreaList;
+import Sprint0.Model.Local;
+import Sprint0.Model.TypeArea;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class US08ControllerTest {
+
+    @Test
+    public void seeIfAreaContainedMatchToAreasOfList(){
+        GeographicAreaList list = new GeographicAreaList();
+        GeographicArea area1 = new GeographicArea("Porto", new TypeArea("Cidade"), new Local(45,45));
+        GeographicArea area2 = new GeographicArea("Portugal", new TypeArea("Pais"), new Local(45,45));
+        GeographicArea area3 = new GeographicArea("Europa", new TypeArea("Continente"), new Local(45,45));
+        list.addGeographicAreaToGeographicAreaList(area1);
+        list.addGeographicAreaToGeographicAreaList(area2);
+        list.addGeographicAreaToGeographicAreaList(area3);
+        US08Controller ctrl = new US08Controller(list);
+
+        ctrl.matchGeographicAreas("Porto","Europa");
+        GeographicArea expectedResult = area1;
+        GeographicArea actualResult = ctrl.getmGeographicAreaContained();
+
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void seeIfAreaContainerMatchToAreasOfList(){
+        GeographicAreaList list = new GeographicAreaList();
+        GeographicArea area1 = new GeographicArea("Porto", new TypeArea("Cidade"), new Local(45,45));
+        GeographicArea area2 = new GeographicArea("Portugal", new TypeArea("Pais"), new Local(45,45));
+        GeographicArea area3 = new GeographicArea("Europa", new TypeArea("Continente"), new Local(45,45));
+        list.addGeographicAreaToGeographicAreaList(area1);
+        list.addGeographicAreaToGeographicAreaList(area2);
+        list.addGeographicAreaToGeographicAreaList(area3);
+        US08Controller ctrl = new US08Controller(list);
+
+        ctrl.matchGeographicAreas("Porto","Europa");
+        GeographicArea expectedResult = area3;
+        GeographicArea actualResult = ctrl.getmGeographicAreaContainer();
+
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void seeIfAreaContainedMatchToAreasOfListFails(){
+        GeographicAreaList list = new GeographicAreaList();
+        GeographicArea area1 = new GeographicArea("Porto", new TypeArea("Cidade"), new Local(45,45));
+        GeographicArea area2 = new GeographicArea("Portugal", new TypeArea("Pais"), new Local(45,45));
+        GeographicArea area3 = new GeographicArea("Europa", new TypeArea("Continente"), new Local(45,45));
+        list.addGeographicAreaToGeographicAreaList(area1);
+        list.addGeographicAreaToGeographicAreaList(area2);
+        list.addGeographicAreaToGeographicAreaList(area3);
+        US08Controller ctrl = new US08Controller(list);
+
+        boolean expectedResult = false;
+        boolean actualResult = ctrl.matchGeographicAreas("Lisboa", "Europa");
+
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void seeIfAreaContainedInAnotherAreaWorks(){
+        GeographicAreaList list = new GeographicAreaList();
+        GeographicArea area1 = new GeographicArea("Porto", new TypeArea("Cidade"), new Local(45,45));
+        GeographicArea area2 = new GeographicArea("Portugal", new TypeArea("Pais"), new Local(45,45));
+        GeographicArea area3 = new GeographicArea("Europa", new TypeArea("Continente"), new Local(45,45));
+        list.addGeographicAreaToGeographicAreaList(area1);
+        list.addGeographicAreaToGeographicAreaList(area2);
+        list.addGeographicAreaToGeographicAreaList(area3);
+        US08Controller ctrl = new US08Controller(list);
+
+        area1.setMotherArea(area2);
+        area2.setMotherArea(area3);
+        ctrl.matchGeographicAreas("Porto","Europa");
+        boolean actualResult = ctrl.seeIfItsContained();
+        boolean expectedResult = true;
+
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void seeIfAreaContainedInAnotherAreaFails(){
+        GeographicAreaList list = new GeographicAreaList();
+        GeographicArea area1 = new GeographicArea("Porto", new TypeArea("Cidade"), new Local(45,45));
+        GeographicArea area2 = new GeographicArea("Portugal", new TypeArea("Pais"), new Local(45,45));
+        GeographicArea area3 = new GeographicArea("Europa", new TypeArea("Continente"), new Local(45,45));
+        list.addGeographicAreaToGeographicAreaList(area1);
+        list.addGeographicAreaToGeographicAreaList(area2);
+        list.addGeographicAreaToGeographicAreaList(area3);
+        US08Controller ctrl = new US08Controller(list);
+
+        area1.setMotherArea(area2);
+        ctrl.matchGeographicAreas("Porto","Europa");
+        boolean actualResult = ctrl.seeIfItsContained();
+        boolean expectedResult = false;
+
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void seeIfAnInvalidListIsAdded(){
+        GeographicAreaList list = new GeographicAreaList();
+        US08Controller ctrl = new US08Controller(list);
+
+        boolean expectedResult = false;
+        boolean actualResult = ctrl.matchGeographicAreas("Porto","Europa");
+
+        assertEquals(expectedResult, actualResult);
+    }
 
 }
