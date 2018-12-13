12f556084b633a0d6b3c1cac8862129fe4a3bdfc
Teresa Varela <1181730@isep.ipp.pt>
2018-12-13 11:22:01 +0000

    ref #3 Class GeographicAreaList: comentários de todos os métodos Class GeographicAreaListTest: implementação de mais testes

--- src/main/java/Sprint0/Model/GeographicAreaList.java
+++ src/main/java/Sprint0/Model/GeographicAreaList.java
@@ -7,15 +7,28 @@ import java.util.List;
 public class GeographicAreaList {
     private List<GeographicArea> mGeographicAreaList;
 
+    /**
+     * GeographicAreaList constructor that receives a Geographic Area as a parameter and
+     * adds the GA to the attribute mGeographicAreaList
+     * @param geographicAreaToAdd geographic area to add to the attribute
+     */
     public GeographicAreaList(GeographicArea geographicAreaToAdd) {
         mGeographicAreaList = new ArrayList<>();
         mGeographicAreaList.add(geographicAreaToAdd);
     }
 
+    /**
+     * GeographicAreaList constructor that receives without parameters
+     */
     public GeographicAreaList() {
         mGeographicAreaList = new ArrayList<>();
     }
 
+    /**
+     * Method that receives a string as a parameter, compares that string with every
+     * Geographic Area name of every GA in the list.
+     * @param areaToMatch string that corresponds to a geographic area name
+     */
     public GeographicArea matchGeoArea (String areaToMatch){
         for (GeographicArea g: mGeographicAreaList){
             if (g.getName().equals(areaToMatch)){
@@ -25,6 +38,23 @@ public class GeographicAreaList {
         return null;
     }
 
+    /**
+     * Method that goes through every geographic area from the attribute mGeographicAreaList
+     * and returns a string with every GA name
+     */
+    public String printGeoAreaList() {
+        String finalString= "Geographic Area List:";
+        for(GeographicArea tipo: mGeographicAreaList)
+            finalString = finalString + " \n"+ "-" + tipo.getName() + ";";
+        return finalString;
+    }
+
+    /**
+     * Method that receives a geographic area as a parameter and adds that
+     * GA to the list in case it is not contained in that list already.
+     * @param geographicAreaToAdd geographic area to be added
+     * @return returns true in case the geographic area is added and false if not
+     * **/
     public boolean addGeographicAreaToGeographicAreaList(GeographicArea geographicAreaToAdd) {
         if(!(mGeographicAreaList.contains(geographicAreaToAdd))) {
             mGeographicAreaList.add(geographicAreaToAdd);
@@ -33,6 +63,12 @@ public class GeographicAreaList {
         return false;
     }
 
+    /**
+     * Method that receives a string as a parameter, compares that string with every
+     * Geographic Area name of every GA in the list and returns true in case of match.
+     * @param geographicAreaToAdd string that corresponds to a geographic area name
+     * @return returns true in case of match and false otherwise
+     */
     public boolean validateIfGeographicAreaToGeographicAreaList(String geographicAreaToAdd) {
         for(GeographicArea ga : mGeographicAreaList) {
             if ((ga.getName().equals(geographicAreaToAdd))) {
@@ -42,26 +78,23 @@ public class GeographicAreaList {
         return false;
     }
 
-    public String printGeoAreaList() {
-        String finalString= "Geographic Area List:";
-        for(GeographicArea tipo: mGeographicAreaList)
-            finalString = finalString + " \n"+ "-" + tipo.getName() + ";";
-        return finalString;
-    }
     /**
-     * Checks if a Geographic Area is inside the Geographic Area List
-     * @param geoArea
-     * @return
+     * Checks if a the Geographic Area given as a parameter is inside the Geographic Area List
+     * @param geoArea geographic area to test
+     * @return returns true in case the GA is contained in the list and false otherwise
      */
     public boolean containsGA(GeographicArea geoArea) {
         return mGeographicAreaList.contains(geoArea);
     }
 
+    /**
+     * Getter of the attribute mGeographicAreaList from this class
+     * @return returns the geographic area list
+     */
     public List<GeographicArea> getGeographicAreaList() {
         return mGeographicAreaList;
     }
 
-    //PARA USAR NA USERSTORY 04??
     public GeographicAreaList matchGeographicAreaWithInputType(String typeOfGeographicArea) {
         GeographicAreaList finalList = new GeographicAreaList();
         TypeArea typeAreaToTest = new TypeArea(typeOfGeographicArea);
--- src/test/java/Sprint0_Test/ModelTest/GeographicAreaListTest.java
+++ src/test/java/Sprint0_Test/ModelTest/GeographicAreaListTest.java
@@ -75,9 +75,93 @@ public class GeographicAreaListTest {
     }
 
     @Test
-    public void seeIfGetGeographicAreaListOfTypeGiven() {
+    public void seeIfGetGeographicAreaListOfSameTypeAsLastGeoAreaInList() {
         //Arrange
         String typeToTest = "Freguesia";
+        TypeArea t1 = new TypeArea("Rua");
+        Local l1 = new Local(38, 7);
+        GeographicArea ga1 = new GeographicArea(t1, l1);
+
+        TypeArea t2 = new TypeArea("País");
+        Local l2 = new Local(43, 71);
+        GeographicArea ga2 = new GeographicArea(t2, l2);
+
+        TypeArea t3 = new TypeArea("Freguesia");
+        Local l3 = new Local(43, 74);
+        GeographicArea ga3 = new GeographicArea(t3, l3);
+
+        GeographicAreaList geographicAreaList = new GeographicAreaList(ga1);
+        geographicAreaList.addGeographicAreaToGeographicAreaList(ga2);
+        geographicAreaList.addGeographicAreaToGeographicAreaList(ga3);
+
+        GeographicAreaList expectedResult = new GeographicAreaList();
+        expectedResult.addGeographicAreaToGeographicAreaList(ga3);
+        GeographicAreaList actualResult;
+        //Act
+        actualResult = geographicAreaList.matchGeographicAreaWithInputType(typeToTest);
+        //Assert
+        assertEquals(expectedResult, actualResult);
+    }
+    @Test
+    public void seeIfGetGeographicAreaListOfSameTypeAsFirstGeoAreaInList() {
+        //Arrange
+        String typeToTest = "Rua";
+        TypeArea t1 = new TypeArea("Rua");
+        Local l1 = new Local(38, 7);
+        GeographicArea ga1 = new GeographicArea(t1, l1);
+
+        TypeArea t2 = new TypeArea("País");
+        Local l2 = new Local(43, 71);
+        GeographicArea ga2 = new GeographicArea(t2, l2);
+
+        TypeArea t3 = new TypeArea("Freguesia");
+        Local l3 = new Local(43, 74);
+        GeographicArea ga3 = new GeographicArea(t3, l3);
+
+        GeographicAreaList geographicAreaList = new GeographicAreaList(ga1);
+        geographicAreaList.addGeographicAreaToGeographicAreaList(ga2);
+        geographicAreaList.addGeographicAreaToGeographicAreaList(ga3);
+
+        GeographicAreaList expectedResult = new GeographicAreaList();
+        expectedResult.addGeographicAreaToGeographicAreaList(ga1);
+        GeographicAreaList actualResult;
+        //Act
+        actualResult = geographicAreaList.matchGeographicAreaWithInputType(typeToTest);
+        //Assert
+        assertEquals(expectedResult, actualResult);
+    }
+    @Test
+    public void seeIfGetGeographicAreaListOfSameTypeAsGeoAreaInMiddleOfList() {
+        //Arrange
+        String typeToTest = "País";
+        TypeArea t1 = new TypeArea("Rua");
+        Local l1 = new Local(38, 7);
+        GeographicArea ga1 = new GeographicArea(t1, l1);
+
+        TypeArea t2 = new TypeArea("País");
+        Local l2 = new Local(43, 71);
+        GeographicArea ga2 = new GeographicArea(t2, l2);
+
+        TypeArea t3 = new TypeArea("Freguesia");
+        Local l3 = new Local(43, 74);
+        GeographicArea ga3 = new GeographicArea(t3, l3);
+
+        GeographicAreaList geographicAreaList = new GeographicAreaList(ga1);
+        geographicAreaList.addGeographicAreaToGeographicAreaList(ga2);
+        geographicAreaList.addGeographicAreaToGeographicAreaList(ga3);
+
+        GeographicAreaList expectedResult = new GeographicAreaList();
+        expectedResult.addGeographicAreaToGeographicAreaList(ga2);
+        GeographicAreaList actualResult;
+        //Act
+        actualResult = geographicAreaList.matchGeographicAreaWithInputType(typeToTest);
+        //Assert
+        assertEquals(expectedResult, actualResult);
+    }
+    @Test
+    public void seeIfGetEmptyGeographicAreaListFromTypeDifferentOfTypeInList() {
+        //Arrange
+        String typeToTest = "Cidade";
         TypeArea t1 = new TypeArea("Rua");
         Local l1 = new Local(38, 7);
         GeographicArea ga1 = new GeographicArea(t1, l1);
@@ -90,7 +174,20 @@ public class GeographicAreaListTest {
         geographicAreaList.addGeographicAreaToGeographicAreaList(ga2);
 
         GeographicAreaList expectedResult = new GeographicAreaList();
-        expectedResult.addGeographicAreaToGeographicAreaList(ga2);
+        GeographicAreaList actualResult;
+        //Act
+        actualResult = geographicAreaList.matchGeographicAreaWithInputType(typeToTest);
+        //Assert
+        assertEquals(expectedResult, actualResult);
+    }
+    @Test
+    public void seeIfGetEmptyGeographicAreaListFromEmptyStartingList() {
+        //Arrange
+        String typeToTest = "Cidade";
+
+        GeographicAreaList geographicAreaList = new GeographicAreaList();
+
+        GeographicAreaList expectedResult = new GeographicAreaList();
         GeographicAreaList actualResult;
         //Act
         actualResult = geographicAreaList.matchGeographicAreaWithInputType(typeToTest);
