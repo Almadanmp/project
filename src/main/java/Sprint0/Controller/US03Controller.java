066e4427242910bc723fec81b5a62524326af958
Daniela <1181701@isep.ipp.pt>
2018-12-13 15:33:56 +0000

    REF #4 US3 - adds javadoc

--- src/main/java/Sprint0/Controller/US03Controller.java
+++ src/main/java/Sprint0/Controller/US03Controller.java
@@ -3,6 +3,8 @@ package Sprint0.Controller;
 import Sprint0.Model.GeographicArea;
 import Sprint0.Model.GeographicAreaList;
 
+import java.util.List;
+
 
 /**
  * User Story 3
@@ -10,11 +12,33 @@ import Sprint0.Model.GeographicAreaList;
  */
 public class US03Controller {
 
-       public boolean addNewGeoArea(GeographicArea geoAToAdd, GeographicAreaList newGeoList) {
-        if (!(newGeoList.containsGA(geoAToAdd))) {
-            newGeoList.addGeographicAreaToGeographicAreaList(geoAToAdd);
-            return true;
+    /**
+     * Method to add a new geographic area to a list of geographic areas
+     *
+     * @param geoAToAdd  Geographic area to add to the list
+     * @param newGeoList Geographic List
+     * @return will return true if the area was added or false if it wasnt added
+     */
+    public boolean addNewGeoArea(GeographicArea geoAToAdd, GeographicAreaList newGeoList) {
+        if (checkIfListValid(newGeoList.getGeographicAreaList())) {
+            if (!(newGeoList.containsGA(geoAToAdd))) {
+                newGeoList.addGeographicAreaToGeographicAreaList(geoAToAdd);
+                return true;
+            }
+
         }
         return false;
+    }
+
+    /**
+     * Method to check if the list of Geographic Areas is Valid
+     * @param values
+     * @return returns false if its not valid (null or empty) or true if valid.
+     */
+    private boolean checkIfListValid(List<GeographicArea> values) {
+        if (values == null || values.isEmpty()) {
+            return false;
+        }
+        return true;
     }
 }
