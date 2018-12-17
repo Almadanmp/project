cca783a449a03e2725d683fc70caa10d0ed27ba4
Teresa Varela <1181730@isep.ipp.pt>
2018-12-17 12:35:48 +0000

    Class US101, US105, US108: sets e gets

---
+++ src/main/java/PT/IPP/ISEP/DEI/Project/Controller/US108Controller.java
@@ -0,0 +1,7 @@
+package PT.IPP.ISEP.DEI.Project.Controller;
+
+/** As an Administrator, I want to have a list of existing rooms, so that I can choose one
+to edit it. **/
+
+public class US108Controller {
+}
