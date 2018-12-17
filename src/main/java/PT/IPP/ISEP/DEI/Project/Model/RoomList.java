636e6dc8590ffb46dc6fd1b40399acd9cf7c028c
Andre Rua <1181689@isep.ipp.pt>
2018-12-17 12:43:42 +0000

    Quick fixes to enable compiler

--- src/main/java/PT/IPP/ISEP/DEI/Project/Model/RoomList.java
+++ src/main/java/PT/IPP/ISEP/DEI/Project/Model/RoomList.java
@@ -1,16 +1,25 @@
 package PT.IPP.ISEP.DEI.Project.Model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class RoomList {
-    List<Room> mRooms;
+    private List<Room> mRooms;
 
     public RoomList () {
-        this.mRooms = new List<Room>();
+        this.mRooms = new ArrayList<>();
     }
-
-    public List<Reading> getListOfRooms() {
+    public RoomList(Room roomToAdd) {
+        mRooms = new ArrayList<>();
+        mRooms.add(roomToAdd);
+    }
+    public List<Room> getListOfRooms() {
         return this.mRooms;
+    }
+    @Override
+    public int hashCode() {
+        return 1;
     }
 
 }
+
