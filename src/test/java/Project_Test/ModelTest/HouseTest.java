c40b2cef3ab3d940f6ff6a9b53e9dee01215ae2d
Daniela <1181701@isep.ipp.pt>
2018-12-17 12:32:43 +0000

    adds new model class - House minor change on javadoc Geographic Area

---
+++ src/test/java/Project_Test/ModelTest/HouseTest.java
@@ -0,0 +1,15 @@
+package Project_Test.ModelTest;
+
+
+import PT.IPP.ISEP.DEI.Project.Model.*;
+import org.junit.jupiter.api.Test;
+
+import java.util.GregorianCalendar;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class HouseTest {
+
+}
+
