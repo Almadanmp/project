cb6be597c6d320b19e982b184ad01d33cfe24c28
1181716 <1181716@isep.ipp.pt>
2018-12-18 13:26:38 +0000

    RoomList Class: code smell

--- src/main/java/PT/IPP/ISEP/DEI/Project/Model/RoomList.java
+++ src/main/java/PT/IPP/ISEP/DEI/Project/Model/RoomList.java
@@ -6,29 +6,33 @@ import java.util.List;
 public class RoomList {
     private List<Room> mRooms;
 
-    public RoomList () {
+    public RoomList() {
         this.mRooms = new ArrayList<>();
     }
+
     public RoomList(Room roomToAdd) {
         mRooms = new ArrayList<>();
         mRooms.add(roomToAdd);
     }
 
-    public boolean doesListOfRoomsContainRoomByName( String name){
-        for (Room room : getListOfRooms()){
+    public boolean doesListOfRoomsContainRoomByName(String name) {
+        for (Room room : getListOfRooms()) {
             if ((room.getRoomName().equals(name))) {
                 return true;
             }
-        }return false;
+        }
+        return false;
     }
 
     public List<Room> getListOfRooms() {
         return this.mRooms;
     }
+
     @Override
     public int hashCode() {
         return 1;
     }
+
     public boolean addRoom(Room room) {
         if (!(mRooms.contains(room))) {
             mRooms.add(room);
