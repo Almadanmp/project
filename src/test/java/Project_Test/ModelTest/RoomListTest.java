088a3d25cb2884130a62b6882f24a1cc83456cb4
Daniela <1181701@isep.ipp.pt>
2018-12-19 16:19:30 +0000

    name change

--- src/test/java/Project_Test/ModelTest/RoomListTest.java
+++ src/test/java/Project_Test/ModelTest/RoomListTest.java
@@ -10,7 +10,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class RoomListTest {
 
     @Test
-    public void seeIfgetRoomByNameFromList() {
+    public void seeIfGetRoomByNameFromList() {
         RoomList roomList = new RoomList();
         Room r1 = new Room("Cozinha", 1, 123, new SensorList(new Sensor("s1", new TypeSensor("Temperatura"), new Local(21, 23), new Date(21 / 11 / 2018))));
         Room r2 = new Room("Jardim", 1, 123, new SensorList(new Sensor("s1", new TypeSensor("Rainfall"), new Local(21, 25), new Date(21 / 11 / 2018))));
