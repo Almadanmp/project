f95e8dad127dec6462db41d99910f430703021d0
danie <1181699@isep.ipp.pt>
2018-12-19 16:40:48 +0000

    Project US135Controller and UI sketched.

--- src/main/java/PT/IPP/ISEP/DEI/Project/Controller/US135Controller.java
+++ src/main/java/PT/IPP/ISEP/DEI/Project/Controller/US135Controller.java
@@ -1,7 +1,21 @@
 package PT.IPP.ISEP.DEI.Project.Controller;
 
+import PT.IPP.ISEP.DEI.Project.Model.PowerSource;
+
 /** As an Administrator, I want to add a power source to a house grid,
  * so that the produced energy may be used by all devices on that grid. **/
 
 public class US135Controller {
+
+    private PowerSource mPowerSource;
+
+    public US135Controller(){}
+
+    public PowerSource createPowerSource(String name, double maxPowerOutput, double maxEnergyStorage){
+        return this.mPowerSource = new PowerSource(name, maxPowerOutput, maxEnergyStorage);
+    }
+
+    public void addPowerSource(PowerSource powerSourceToAdd){
+        this.mPowerSource = powerSourceToAdd;
+    }
 }
--- src/main/java/PT/IPP/ISEP/DEI/Project/IO/UI/US135UI.java
+++ src/main/java/PT/IPP/ISEP/DEI/Project/IO/UI/US135UI.java
@@ -1,8 +1,38 @@
 package PT.IPP.ISEP.DEI.Project.IO.UI;
 
-/**As an Administrator, I want to add a new room to the house, in order to configure
-it (name, house floor and dimensions).
- **/
+import PT.IPP.ISEP.DEI.Project.Controller.US135Controller;
+import PT.IPP.ISEP.DEI.Project.Model.EnergyGrid;
+import PT.IPP.ISEP.DEI.Project.Model.PowerSource;
+
+import java.util.Scanner;
+
+/** As an Administrator, I want to add a power source to a energy grid,
+ * so that the produced energy may be used by all devices on that grid. **/
 
 public class US135UI {
+
+    private boolean mActive;
+
+    public US135UI(){mActive = false;}
+
+    public void run(EnergyGrid energyGrid){
+        this.mActive = true;
+        while(this.mActive){
+            addPowerSourceToEnergyGrid();
+            //displayState();
+        }
+    }
+
+    public void addPowerSourceToEnergyGrid(){
+        US135Controller ctrl = new US135Controller();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Type the designation of the power source you want to add: ");
+        String name = scanner.next();
+        System.out.println("Type the maximum power output of the power source you want to add: ");
+        double maxPowerOutput = scanner.nextDouble();
+        System.out.println("Type the maximum energy storage of the power source you want to add (type 0 if the power source can't storage energy.): ");
+        double maxEnergyStorage = scanner.nextDouble();
+        PowerSource templateToAdd = ctrl.createPowerSource(name, maxPowerOutput, maxEnergyStorage);
+        ctrl.addPowerSource(templateToAdd);
+    }
 }
