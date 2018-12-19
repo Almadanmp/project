fc0f59a0333f444c77f611543b3e313eba278469
danie <1181699@isep.ipp.pt>
2018-12-19 10:16:11 +0000

    Project - HouseGrid: Added needed functionalities( attach rooms, change MaxContractedPower

--- src/main/java/PT/IPP/ISEP/DEI/Project/Model/HouseGridList.java
+++ src/main/java/PT/IPP/ISEP/DEI/Project/Model/HouseGridList.java
@@ -1,9 +1,18 @@
 package PT.IPP.ISEP.DEI.Project.Model;
 
+import java.util.*;
+
 /**
  * House Class. Defines the List of HouseGrids
  */
 public class HouseGridList {
+
+    private List<HouseGrid> mHouseGridList;
+
+
+
+    public HouseGridList() {this.mHouseGridList = new ArrayList<>();}
+
 
 }
 
