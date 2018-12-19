a6534431a5d2dfb9e90e01f0b7fb38080f172b17
1181716 <1181716@isep.ipp.pt>
2018-12-19 10:02:49 +0000

    #31 US101UI code smells + main ui info added

--- src/main/java/PT/IPP/ISEP/DEI/Project/IO/UI/MainUI.java
+++ src/main/java/PT/IPP/ISEP/DEI/Project/IO/UI/MainUI.java
@@ -67,6 +67,7 @@ public class MainUI {
                     "8. See if an area is contained in another area.\n",
                     "9. Display all available sensors.\n",
                     "10. Display all available Geographic Areas.\n",
+                    "11. Configure a house.\n",
                     "20. Menu for Admin only test.\n"};
 
             System.out.println("Select the task you want to do:");
