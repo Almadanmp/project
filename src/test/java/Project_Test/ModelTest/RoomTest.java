775fc8cd21200b4cd247f4ba2ec2cc82ecd00053
Dark <1181722@isep.ipp.pt>
2018-12-20 11:00:55 +0000

    Acrescentado testes para getCurrentRoomTemperature

--- src/test/java/Project_Test/ModelTest/RoomTest.java
+++ src/test/java/Project_Test/ModelTest/RoomTest.java
@@ -11,47 +11,111 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class RoomTest {
 
     @Test
-    public void seeIfgetMaxTemperatureInARoomOnAGivenDayWorks(){
+    public void seeIfgetMaxTemperatureInARoomOnAGivenDayWorks() {
         SensorList list = new SensorList();
         TypeSensor tipo = new TypeSensor("temperature");
         ReadingList listR = new ReadingList();
-        Date d2 = new GregorianCalendar(2018,2,2).getTime();
+        Date d2 = new GregorianCalendar(2018, 2, 2).getTime();
         Reading r1;
         Reading r2;
-        r1 = new Reading(30,d2);
+        r1 = new Reading(30, d2);
         r2 = new Reading(20, d2);
         listR.addReading(r1);
         listR.addReading(r2);
-        Sensor s1  = new Sensor("sensor1",tipo,new Local(1,1),new Date(),listR);
+        Sensor s1 = new Sensor("sensor1", tipo, new Local(1, 1), new Date(), listR);
         list.addSensor(s1);
-        Room room = new Room("quarto",1,80, list);
+        Room room = new Room("quarto", 1, 80, list);
         double result = room.getMaxTemperatureInARoomOnAGivenDay(d2);
         double expectedResult = 30.0;
-        assertEquals(expectedResult,result,0.01);
-
-
+        assertEquals(expectedResult, result, 0.01);
     }
+
     @Test
-    public void seeIfGetCurrentRoomTemperatureWorks(){
+    public void seeIfGetCurrentRoomTemperatureWorks() {
         SensorList list = new SensorList();
         TypeSensor type = new TypeSensor("temperature");
         ReadingList listR = new ReadingList();
-        Date d2 = new GregorianCalendar(2018,2,1,16,30).getTime();
-        Date d3 = new GregorianCalendar(2018,2,1,20,30).getTime();
+        Date d1 = new GregorianCalendar(2018, 2, 1, 16, 30).getTime();
+        Date d2 = new GregorianCalendar(2018, 2, 1, 20, 30).getTime();
+        Date d3 = new GregorianCalendar(2018, 1, 1, 20, 30).getTime();
+        Date d4 = new GregorianCalendar(2017, 12, 1, 20, 30).getTime();
         Reading r1;
         Reading r2;
-        r1 = new Reading(30,d2);
-        r2 = new Reading(20, d3);
+        Reading r3;
+        Reading r4;
+        r1 = new Reading(30, d1);
+        r2 = new Reading(20, d2);
+        r3 = new Reading(25, d3);
+        r4 = new Reading(10, d4);
         listR.addReading(r1);
         listR.addReading(r2);
-        Sensor s1  = new Sensor("sensor1",type,new Local(1,1),new Date(),listR);
+        listR.addReading(r3);
+        listR.addReading(r4);
+        Sensor s1 = new Sensor("sensor1", type, new Local(1, 1), new Date(), listR);
         list.addSensor(s1);
-        Room room = new Room("quarto",1,80, list);
-        double result = room.getCurrentRoomTemperature(d2);
+        Room room = new Room("quarto", 1, 80, list);
+        double result = room.getCurrentRoomTemperature(d1);
         double expectedResult = 20.0;
-        assertEquals(expectedResult,result,0.01);
+        assertEquals(expectedResult, result, 0.01);
+    }
 
+    @Test
+    public void seeIfGetCurrentRoomTemperatureWorksNegative() {
+        SensorList list = new SensorList();
+        TypeSensor type = new TypeSensor("temperature");
+        ReadingList listR = new ReadingList();
+        Date d1 = new GregorianCalendar(2018, 2, 1, 16, 30).getTime();
+        Date d2 = new GregorianCalendar(2018, 2, 1, 20, 30).getTime();
+        Date d3 = new GregorianCalendar(2018, 1, 1, 20, 30).getTime();
+        Date d4 = new GregorianCalendar(2017, 12, 1, 20, 30).getTime();
+        Reading r1;
+        Reading r2;
+        Reading r3;
+        Reading r4;
+        r1 = new Reading(30, d1);
+        r2 = new Reading(-20, d2);
+        r3 = new Reading(25, d3);
+        r4 = new Reading(10, d4);
+        listR.addReading(r1);
+        listR.addReading(r2);
+        listR.addReading(r3);
+        listR.addReading(r4);
+        Sensor s1 = new Sensor("sensor1", type, new Local(1, 1), new Date(), listR);
+        list.addSensor(s1);
+        Room room = new Room("quarto", 1, 80, list);
+        double result = room.getCurrentRoomTemperature(d1);
+        double expectedResult = -20.0;
+        assertEquals(expectedResult, result, 0.01);
+    }
 
+    @Test
+    public void seeIfGetCurrentRoomTemperatureWorksMinute() {
+        SensorList list = new SensorList();
+        TypeSensor type = new TypeSensor("temperature");
+        ReadingList listR = new ReadingList();
+        Date d1 = new GregorianCalendar(2018, 2, 1, 20, 31).getTime();
+        Date d2 = new GregorianCalendar(2018, 2, 1, 20, 30).getTime();
+        Date d3;
+        d3 = new GregorianCalendar(2018, 1, 1, 20, 30).getTime();
+        Date d4 = new GregorianCalendar(2017, 12, 1, 20, 30).getTime();
+        Reading r1;
+        Reading r2;
+        Reading r3;
+        Reading r4;
+        r1 = new Reading(30, d1);
+        r2 = new Reading(-20, d2);
+        r3 = new Reading(25, d3);
+        r4 = new Reading(10, d4);
+        listR.addReading(r1);
+        listR.addReading(r2);
+        listR.addReading(r3);
+        listR.addReading(r4);
+        Sensor s1 = new Sensor("sensor1", type, new Local(1, 1), new Date(), listR);
+        list.addSensor(s1);
+        Room room = new Room("quarto", 1, 80, list);
+        double result = room.getCurrentRoomTemperature(d1);
+        double expectedResult = 30.0;
+        assertEquals(expectedResult, result, 0.01);
     }
 
 }
