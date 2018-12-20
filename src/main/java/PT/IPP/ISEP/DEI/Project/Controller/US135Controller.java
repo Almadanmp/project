faa8d9435fc408f194d8e22bc3ce804f0b7bac45
danie <1181699@isep.ipp.pt>
2018-12-20 12:18:17 +0000

    #35 US135 Functionality implemented and organized.

--- src/main/java/PT/IPP/ISEP/DEI/Project/Controller/US135Controller.java
+++ src/main/java/PT/IPP/ISEP/DEI/Project/Controller/US135Controller.java
@@ -1,21 +1,18 @@
 package PT.IPP.ISEP.DEI.Project.Controller;
 
 import PT.IPP.ISEP.DEI.Project.Model.EnergyGrid;
-import PT.IPP.ISEP.DEI.Project.Model.EnergyGridList;
 import PT.IPP.ISEP.DEI.Project.Model.PowerSource;
 
-/** As an Administrator, I want to add a power source to a house grid,
+/** As an Administrator, I want to add a power source to an energy grid,
  * so that the produced energy may be used by all devices on that grid. **/
 
 public class US135Controller {
 
+    private PowerSource mPowerSource;
+
     public US135Controller(){}
 
-    public PowerSource createPowerSource(String name, double maxPowerOutput, double maxEnergyStorage){
-        return new PowerSource(name, maxPowerOutput, maxEnergyStorage);
-    }
+    public void createPowerSource(String name, double maxPowerOutput, double maxEnergyStorage){this.mPowerSource = new PowerSource(name, maxPowerOutput, maxEnergyStorage);}
 
-    public void addPowerSourceToEnergyGrid(EnergyGrid energyGrid, PowerSource powerSource){
-        energyGrid.addPowerSource(powerSource);
-    }
+    public void addPowerSourceToEnergyGrid(EnergyGrid energyGrid){energyGrid.addPowerSource(mPowerSource);}
 }
