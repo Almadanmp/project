f6adad9c99587c9e25ff15db89307fa87e9b6080
Daniela <1181701@isep.ipp.pt>
2018-12-20 15:35:48 +0000

    REF #27 Improvements on US623

--- src/main/java/PT/IPP/ISEP/DEI/Project/Controller/US623Controller.java
+++ src/main/java/PT/IPP/ISEP/DEI/Project/Controller/US623Controller.java
@@ -4,6 +4,7 @@ import PT.IPP.ISEP.DEI.Project.Model.GeographicArea;
 import PT.IPP.ISEP.DEI.Project.Model.House;
 
 import java.util.Date;
+import java.util.GregorianCalendar;
 
 /**
  * US623UI: As a Regular User, I want to get the average daily rainfall in the house area for a
@@ -11,10 +12,23 @@ import java.util.Date;
  **/
 
 public class US623Controller {
+    private Date mDate;
+
+    public GeographicArea getGeographicArea(String geographicAreaName) {
+        GeographicArea geo = new GeographicArea();
+        return geo.getmGeographicAreaList().getGeographicAreaByName(geographicAreaName);
+
+    }
 
     public double getAVGDailyRainfallOnGivenPeriod(House house, Date minDay, Date maxDay) {
         GeographicArea geoAreaOfTheHouse = house.getmMotherGA();
         return geoAreaOfTheHouse.getAvgReadingsFromSensorTypeInGA("rainfall", minDay, maxDay);
+    }
+
+    public Date createDate(int year, int month, int day) {
+        Date date = new GregorianCalendar(year, month, day).getTime();
+        this.mDate = date;
+        return this.mDate;
     }
 }
 
--- src/main/java/PT/IPP/ISEP/DEI/Project/IO/UI/US623UI.java
+++ src/main/java/PT/IPP/ISEP/DEI/Project/IO/UI/US623UI.java
@@ -1,8 +1,15 @@
 package PT.IPP.ISEP.DEI.Project.IO.UI;
 
-import PT.IPP.ISEP.DEI.Project.Model.*;
+import PT.IPP.ISEP.DEI.Project.Controller.US623Controller;
+import PT.IPP.ISEP.DEI.Project.Model.GeographicArea;
+import PT.IPP.ISEP.DEI.Project.Model.GeographicAreaList;
+import PT.IPP.ISEP.DEI.Project.Model.House;
+import PT.IPP.ISEP.DEI.Project.Model.HouseList;
 
+import java.util.Date;
 import java.util.Scanner;
+
+import static java.lang.System.out;
 
 /**
  * US623UI: As a Regular User, I want to get the average daily rainfall in the house area for a
@@ -12,52 +19,113 @@ public class US623UI {
     private boolean active;
     private Scanner mScanner;
     private House mHouse;
+    private String mGeoAreaName;
     private GeographicArea mGeoArea;
-
+    private double mResult;
+    private Date mStartDate;
+    private Date mEndDate;
+    private int dataYear1;
+    private int dataMonth1;
+    private int dataDay1;
+    private int dataYear2;
+    private int dataMonth2;
+    private int dataDay2;
+    private US623Controller controller;
 
 
     public US623UI() {
         this.mScanner = new Scanner(System.in);
+        this.controller = new US623Controller();
     }
 
     public void run(GeographicAreaList newGeoListUi, HouseList newHouseListUi) {
         this.active = true;
         getInputGeographicArea(newGeoListUi);
         getInputHouse(newHouseListUi);
-      //  getInputRoom();
-       // getInputDate();
-        //updateModel();
-       // displayState();
+        getInputStartDate();
+        getInputEndDate();
+        updateModelUS620();
+        updateModelUS623(mGeoAreaName, mHouse);
+        displayState();
+
     }
 
     private void getInputGeographicArea(GeographicAreaList newGeoListUi) {
         System.out.println("Please Select One Of The Existing Geographic Areas: " + newGeoListUi.printGeoAreaList());
-        String  geoArea = mScanner.nextLine();
-        mGeoArea = newGeoListUi.getGeographicAreaByName(geoArea);
-        }
+        mGeoAreaName = mScanner.nextLine();
+    }
 
     private void getInputHouse(HouseList newHouseListUi) {
-        System.out.println("Please Select One Of The Existing Houses on " + mGeoArea);
-        String  house = mScanner.nextLine();
+        System.out.println("Please Select One Of The Existing Houses on " + mGeoAreaName + "\n" + newHouseListUi.printHouseList());
+        String house = mScanner.nextLine();
         mHouse = newHouseListUi.getHouseByDesignation(house);
     }
 
-    private String createInputMsg(String inputType) {
-        return "Please Insert " + inputType + " for the New Geographic Area: ";
-    }
-
-    private String createInvalidStringMsg(String inputType) {
-        return "That's not a valid " + inputType + ". Please insert only Alphabetic Characters";
-    }
-
-    private String readInputString(String inputType) {
-        System.out.print(createInputMsg(inputType));
-
-        while (!mScanner.hasNext("[a-zA-Z\\sà-ùÀ-Ù]*")) {
-            System.out.println(createInvalidStringMsg(inputType));
+    private void getInputStartDate() {
+        System.out.println("Enter the year:");
+        while (!mScanner.hasNextInt()) {
             mScanner.next();
+            out.println("Not a valid year. Try again");
         }
-        return mScanner.next();
+        this.dataYear1 = mScanner.nextInt();
+        mScanner.nextLine();
+        out.println("\nEnter the Month:\t");
+        while (!mScanner.hasNextInt()) {
+            mScanner.next();
+            out.println("Not a valid month. Try again");
+        }
+        this.dataMonth1 = mScanner.nextInt();
+        mScanner.nextLine();
+        out.println("\nEnter the Day:\t");
+        while (!mScanner.hasNextInt()) {
+            mScanner.next();
+            out.println("Not a valid day. Try again");
+        }
+        this.dataDay1 = mScanner.nextInt();
+        out.println("You entered the date successfully!");
+        mScanner.nextLine();
+    }
+
+    private void getInputEndDate() {
+        System.out.println("Enter the year:");
+        while (!mScanner.hasNextInt()) {
+            mScanner.next();
+            out.println("Not a valid year. Try again");
+        }
+        this.dataYear2 = mScanner.nextInt();
+        mScanner.nextLine();
+        out.println("\nEnter the Month:\t");
+        while (!mScanner.hasNextInt()) {
+            mScanner.next();
+            out.println("Not a valid month. Try again");
+        }
+        this.dataMonth2 = mScanner.nextInt();
+        mScanner.nextLine();
+        out.println("\nEnter the Day:\t");
+        while (!mScanner.hasNextInt()) {
+            mScanner.next();
+            out.println("Not a valid day. Try again");
+        }
+        this.dataDay2 = mScanner.nextInt();
+        out.println("You entered the date successfully!");
+        mScanner.nextLine();
+    }
+
+    private void updateModelUS620() {
+
+    }
+
+    private void updateModelUS623(String mGeoArea, House mHouse) {
+        this.mGeoArea = controller.getGeographicArea(mGeoArea);
+        this.mStartDate = controller.createDate(dataDay1, dataMonth1, dataYear1);
+        this.mEndDate = controller.createDate(dataDay2, dataMonth2, dataYear2);
+        this.mResult = controller.getAVGDailyRainfallOnGivenPeriod(mHouse, mStartDate, mEndDate);
+    }
+
+    private void displayState() {
+        System.out.print("The Average Temperature on " + mHouse + "that is located on " + mGeoAreaName + "between the dates" +
+                mStartDate + "and " + mEndDate + "is " + mResult + "%.");
+        active = false;
     }
 }
 
