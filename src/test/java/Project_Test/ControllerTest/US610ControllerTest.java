5c687c7f316e9bfe8d8312fa5da9fc1296438f1b
Cárina <carina.alas11@gmail.com>
2018-12-21 09:59:37 +0000

    #26 - more tests added to 'US610Controller'

--- src/test/java/Project_Test/ControllerTest/US610ControllerTest.java
+++ src/test/java/Project_Test/ControllerTest/US610ControllerTest.java
@@ -66,4 +66,82 @@ class US610ControllerTest {
         assertEquals(expectedResult,result);
     }
 
+    @Test
+    public void seeIfGetMaxTemperatureInARoomInAGivenDayWorks(){
+        RoomList roomList = new RoomList();
+        SensorList list = new SensorList();
+        TypeSensor tipo = new TypeSensor("temperature");
+        ReadingList listR = new ReadingList();
+        Date d2 = new GregorianCalendar(2018, 2, 2).getTime();
+        Reading r1;
+        Reading r2;
+        r1 = new Reading(30, d2);
+        r2 = new Reading(20, d2);
+        listR.addReading(r1);
+        listR.addReading(r2);
+        Sensor s1 = new Sensor("sensor1", tipo, new Local(1, 1), new Date(), listR);
+        list.addSensor(s1);
+        Room room = new Room("quarto", 1, 80, list);
+        roomList.addRoom(room);
+        US610Controller ctrl = new US610Controller(roomList);
+        double result = ctrl.getMaxTemperatureInARoomOnAGivenDay(d2);
+        double expectedResult = 30.0;
+        assertEquals(expectedResult, result, 0.01);
+    }
+
+    @Test
+    public void seeIfGetMaxTemperatureInARoomInAGivenDayWorksTwoDays(){
+        RoomList roomList = new RoomList();
+        SensorList list = new SensorList();
+        TypeSensor tipo = new TypeSensor("temperature");
+        ReadingList listR = new ReadingList();
+        Date d2 = new GregorianCalendar(2018, 2, 2).getTime();
+        Date d3 = new GregorianCalendar(2018, 2, 3).getTime();
+        Reading r1 = new Reading(-30, d2);
+        Reading r2 = new Reading(20, d2);
+        Reading r3 = new Reading(25,d3);
+        listR.addReading(r1);
+        listR.addReading(r2);
+        listR.addReading(r3);
+        Sensor s1 = new Sensor("sensor1", tipo, new Local(1, 1), new Date(), listR);
+        list.addSensor(s1);
+        Room room = new Room("quarto", 1, 80, list);
+        roomList.addRoom(room);
+        US610Controller ctrl = new US610Controller(roomList);
+        double result = ctrl.getMaxTemperatureInARoomOnAGivenDay(d3);
+        double expectedResult = 25.0;
+        assertEquals(expectedResult, result, 0.01);
+    }
+    @Test
+    public void seeIfGetMaxTemperatureInARoomInAGivenDayWorksTwoDaysNeg(){
+        RoomList roomList = new RoomList();
+        SensorList list = new SensorList();
+        TypeSensor tipo = new TypeSensor("temperature");
+        ReadingList listR = new ReadingList();
+        Date d2 = new GregorianCalendar(2018, 2, 2).getTime();
+        Date d3 = new GregorianCalendar(2018, 2, 3).getTime();
+        Reading r1 = new Reading(-30, d2);
+        Reading r2 = new Reading(-20, d2);
+        Reading r3 = new Reading(-25, d3);
+        listR.addReading(r1);
+        listR.addReading(r2);
+        listR.addReading(r3);
+        Sensor s1 = new Sensor("sensor1", tipo, new Local(1, 1), new Date(), listR);
+        list.addSensor(s1);
+        Room room = new Room("quarto", 1, 80, list);
+        roomList.addRoom(room);
+        US610Controller ctrl = new US610Controller(roomList);
+        double result = ctrl.getMaxTemperatureInARoomOnAGivenDay(d2);
+        double expectedResult = -20.0;
+        assertEquals(expectedResult, result, 0.01);
+    }
+
+    @Test
+    public void SeeIfCreateDateWorks(){
+        RoomList roomList = new RoomList();
+        US610Controller ctrl = new US610Controller(roomList);
+        Date result = ctrl.createDate(2018,6,11);
+        Date expectedResult = new GregorianCalendar(2018,6,11).getTime();
+        assertEquals(expectedResult,result);
+    }
 }
