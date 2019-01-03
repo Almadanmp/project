73cda7a138630d6d7bc2cb345a6ffd3343d5902c
Dark <1181722@isep.ipp.pt>
2019-01-03 18:37:46 +0000

    DeviceListTest Finished adding all tests.

--- src/test/java/pt/ipp/isep/dei/project/model/DeviceListTest.java
+++ src/test/java/pt/ipp/isep/dei/project/model/DeviceListTest.java
@@ -35,6 +35,30 @@ public class DeviceListTest {
     }
 
     @Test
+    public void seeIfContainsDeviceWorksForFalse() {
+        //Arrange --------------------------
+        //Room List
+        RoomList roomList1 = new RoomList();
+        Room room1 = new Room("room1", 19, 23456789);
+        roomList1.addRoom(room1);
+        //Reading List
+        ReadingList rL1 = new ReadingList();
+        Date d2 = new GregorianCalendar(2018, 2, 2).getTime();
+        Reading r1;
+        r1 = new Reading(30, d2);
+        rL1.addReading(r1);
+        //Total Power
+        double tP1 = 50;
+        //Device List
+        DeviceList deviceL1 = new DeviceList();
+        Device d1 = new Device("Television", "House Apliance", room1, rL1, tP1);
+        //Act ------------------------------
+        boolean result = deviceL1.containsDevice(d1);
+        //Assert ---------------------------
+        assertFalse(result);
+    }
+
+    @Test
     public void seeIfRemoveDeviceWorks() {
         //Arrange ------------------------
         //Room List
@@ -57,6 +81,77 @@ public class DeviceListTest {
         deviceL1.removeDevice(d1);
         boolean result = deviceL1.containsDevice(d1);
         //Assert -------------------------
+        assertFalse(result);
+    }
+
+    @Test
+    public void seeIfRemoveDeviceWorksForFalse() {
+        //Arrange ------------------------
+        //Room List
+        RoomList roomList1 = new RoomList();
+        Room room1 = new Room("room1", 19, 23456789);
+        roomList1.addRoom(room1);
+        //Reading List
+        ReadingList rL1 = new ReadingList();
+        Date d2 = new GregorianCalendar(2018, 2, 2).getTime();
+        Reading r1;
+        r1 = new Reading(30, d2);
+        rL1.addReading(r1);
+        //Total Power
+        double tP1 = 50;
+        //Device List
+        DeviceList deviceL1 = new DeviceList();
+        Device d1 = new Device("Television", "House Apliance", room1, rL1, tP1);
+        deviceL1.addDevices(d1);
+        //Act ----------------------------
+        boolean result = deviceL1.containsDevice(d1);
+        //Assert -------------------------
+        assertTrue(result);
+    }
+
+    @Test
+    public void seeIfCheckIfListIsValidWorks() {
+        //Arrange ------------------------------
+        //Room List
+        RoomList roomList1 = new RoomList();
+        Room room1 = new Room("room1", 19, 23456789);
+        roomList1.addRoom(room1);
+        //Reading List
+        ReadingList rL1 = new ReadingList();
+        Date d2 = new GregorianCalendar(2018, 2, 2).getTime();
+        Reading r1;
+        r1 = new Reading(30, d2);
+        rL1.addReading(r1);
+        //Total Power
+        double tP1 = 50;
+        //Device List
+        DeviceList deviceL1 = new DeviceList();
+        Device d1 = new Device("Television", "House Apliance", room1, rL1, tP1);
+        deviceL1.addDevices(d1);
+        //Act ----------------------------------
+        boolean result = deviceL1.checkIfListIsValid();
+        //Assert -------------------------------
+        assertTrue(result);
+    }
+
+    @Test
+    public void seeIfCheckIfListIsValidWorksForFalse() {
+        //Arrange ------------------------------
+        //Room List
+        RoomList roomList1 = new RoomList();
+        Room room1 = new Room("room1", 19, 23456789);
+        roomList1.addRoom(room1);
+        //Reading List
+        ReadingList rL1 = new ReadingList();
+        Date d2 = new GregorianCalendar(2018, 2, 2).getTime();
+        Reading r1;
+        r1 = new Reading(30, d2);
+        rL1.addReading(r1);
+        //Device List
+        DeviceList deviceL1 = new DeviceList();
+        //Act ----------------------------------
+        boolean result = deviceL1.checkIfListIsValid();
+        //Assert -------------------------------
         assertFalse(result);
     }
 }
