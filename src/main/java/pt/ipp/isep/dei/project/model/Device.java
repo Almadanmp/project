a29a3c714ba5679b05130a7e7a3c70248c357ac1
Daniela <1181701@isep.ipp.pt>
2019-01-03 15:42:37 +0000

    removes unused methods from device and house

--- src/main/java/pt/ipp/isep/dei/project/model/Device.java
+++ src/main/java/pt/ipp/isep/dei/project/model/Device.java
@@ -15,32 +15,16 @@ public class Device {
         setmTotalPowerDevice(totalPower);
     }
 
-    public String getmName(){
-        return this.mName;
-    }
-
     public void setmName(String name){
         this.mName=name;
-    }
-
-    public String getmDeviceType(){
-        return this.mDeviceType;
     }
 
     public void setmDeviceType(String type){
         this.mDeviceType=type;
     }
 
-    public Room getmParentRoom(){
-        return this.mParentRoom;
-    }
-
     public void setmParentRoom(Room parentRoom){
         this.mParentRoom=parentRoom;
-    }
-
-    public ReadingList getmEnergyConsumptionList(){
-        return mEnergyConsumptionList;
     }
 
     public void setmEnergyConsumptionList(ReadingList list){
