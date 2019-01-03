3ffbd1caa9a847d2d79c6aaa35f43d843dd1543e
Dark <1181722@isep.ipp.pt>
2019-01-03 18:26:06 +0000

    DeviceListTest Added tests

--- src/test/java/pt/ipp/isep/dei/project/model/DeviceListTest.java
+++ src/test/java/pt/ipp/isep/dei/project/model/DeviceListTest.java
@@ -1,6 +1,62 @@
 package pt.ipp.isep.dei.project.model;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class DeviceListTest {
 
+    @Test
+    public void seeIfContainsDeviceWorks() {
+        //Arrange --------------------------
+        //Room List
+        RoomList roomList1 = new RoomList();
+        Room room1 = new Room("room1", 19, 23456789);
+        roomList1.addRoom(room1);
+        //Reading List
+        ReadingList rL1 = new ReadingList();
+        Date d2 = new GregorianCalendar(2018, 2, 2).getTime();
+        Reading r1;
+        r1 = new Reading(30, d2);
+        rL1.addReading(r1);
+        //Total Power
+        double tP1 = 50;
+        //Device List
+        DeviceList deviceL1 = new DeviceList();
+        Device d1 = new Device("Television", "House Apliance", room1, rL1, tP1);
+        deviceL1.addDevices(d1);
+        //Act ------------------------------
+        boolean result = deviceL1.containsDevice(d1);
+        //Assert ---------------------------
+        assertTrue(result);
+    }
 
+    @Test
+    public void seeIfRemoveDeviceWorks() {
+        //Arrange ------------------------
+        //Room List
+        RoomList roomList1 = new RoomList();
+        Room room1 = new Room("room1", 19, 23456789);
+        roomList1.addRoom(room1);
+        //Reading List
+        ReadingList rL1 = new ReadingList();
+        Date d2 = new GregorianCalendar(2018, 2, 2).getTime();
+        Reading r1;
+        r1 = new Reading(30, d2);
+        rL1.addReading(r1);
+        //Total Power
+        double tP1 = 50;
+        //Device List
+        DeviceList deviceL1 = new DeviceList();
+        Device d1 = new Device("Television", "House Apliance", room1, rL1, tP1);
+        deviceL1.addDevices(d1);
+        //Act ----------------------------
+        deviceL1.removeDevice(d1);
+        boolean result = deviceL1.containsDevice(d1);
+        //Assert -------------------------
+        assertFalse(result);
+    }
 }
