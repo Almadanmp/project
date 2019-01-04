627835f854d9738ab0df02b96dfe303ff78d9142
Dark <1181722@isep.ipp.pt>
2019-01-04 10:55:03 +0000

    Added tests for hash and equals.

--- src/test/java/pt/ipp/isep/dei/project/model/PowerSourceListTest.java
+++ src/test/java/pt/ipp/isep/dei/project/model/PowerSourceListTest.java
@@ -3,6 +3,7 @@ package pt.ipp.isep.dei.project.model;
 import org.junit.jupiter.api.Assertions;
 import org.testng.annotations.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.testng.Assert.*;
 
 public class PowerSourceListTest {
@@ -21,16 +22,59 @@ public class PowerSourceListTest {
     }
 
     @Test
-    public void seeIfEquals() {
+    public void seeHashCodeDummyTest() {
+        PowerSource pS1 = new PowerSource("Energia", 50, 50);
+        PowerSourceList pSList1 = new PowerSourceList();
+        pSList1.addPowerSource(pS1);
+        int expectedResult = 1;
+        int actualResult = pSList1.hashCode();
+        assertEquals(expectedResult, actualResult);
     }
 
     @Test
-    public void hashCodeDummyTest() {
-        PowerSourceList powerSourceList = new PowerSourceList();
-        PowerSource power = new PowerSource("power", 19, 289);
-        powerSourceList.addPowerSource(power);
-        int expectedResult = 1;
-        int actualResult = powerSourceList.hashCode();
-        Assertions.assertEquals(expectedResult, actualResult);
+    public void seeIfEqualsPowerSourceWithDifferentObject() {
+        PowerSource pS1 = new PowerSource("Energia", 50, 50);
+        int teste = 3;
+        PowerSourceList pSList1 = new PowerSourceList();
+        pSList1.addPowerSource(pS1);
+        boolean actualResult = pSList1.equals(teste);
+        boolean expectedResult = false;
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void seeIfEqualsPowerSourceWithDifferentContent() {
+        PowerSource pS1 = new PowerSource("Energia", 50, 50);
+        PowerSource pS2 = new PowerSource("Muita Energia", 50, 50);
+        PowerSourceList pSList1 = new PowerSourceList();
+        PowerSourceList pSList2 = new PowerSourceList();
+        pSList1.addPowerSource(pS1);
+        pSList2.addPowerSource(pS2);
+        boolean actualResult = pSList1.equals(pSList2);
+        boolean expectedResult = false;
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void seeIfEqualsPowerSourceWithSameContent() {
+        PowerSource pS1 = new PowerSource("Energia", 50, 50);
+        PowerSource pS2 = new PowerSource("Energia", 50, 50);
+        PowerSourceList pSList1 = new PowerSourceList();
+        PowerSourceList pSList2 = new PowerSourceList();
+        pSList1.addPowerSource(pS1);
+        pSList2.addPowerSource(pS2);
+        boolean actualResult = pSList1.equals(pSList2);
+        boolean expectedResult = true;
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void seeIfequalsSameObject() {
+        PowerSource pS1 = new PowerSource("Energia", 50, 50);
+        PowerSourceList pSList1 = new PowerSourceList();
+        pSList1.addPowerSource(pS1);
+        boolean actualResult = pSList1.equals(pSList1);
+        boolean expectedResult = true;
+        assertEquals(expectedResult, actualResult);
     }
 }
