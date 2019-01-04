31d1c1945f48df5f86d5a08f274a06fe551de499
Teresa Varela <1181730@isep.ipp.pt>
2019-01-04 09:47:40 +0000

    DELETE US108

--- src/main/java/pt/ipp/isep/dei/project/io/ui/HouseConfigurationUI.java
+++ src/main/java/pt/ipp/isep/dei/project/io/ui/HouseConfigurationUI.java
@@ -6,7 +6,6 @@ import pt.ipp.isep.dei.project.model.*;
 import java.util.Date;
 import java.util.List;
 import java.util.Scanner;
-import java.util.stream.Collectors;
 
 class HouseConfigurationUI {
     private HouseConfigurationController controller;
