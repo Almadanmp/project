96777d1e4572442553dd8cba00e80ea7ff5851fe
Dark <1181722@isep.ipp.pt>
2019-01-09 15:12:58 +0000

    Local constructor use corrected For when not-full constructor gets deleted.

--- src/test/java/pt/ipp/isep/dei/project/model/RoomListTest.java
+++ src/test/java/pt/ipp/isep/dei/project/model/RoomListTest.java
@@ -14,7 +14,7 @@ public class RoomListTest {
     @Test
     public void seeIfGetRoomByNameFromList() {
         RoomList roomList = new RoomList();
-        SensorList sensorList = new SensorList(new Sensor("s1", new TypeSensor("Temperatura"), new Local(21, 23), new Date(21 / 11 / 2018)));
+        SensorList sensorList = new SensorList(new Sensor("s1", new TypeSensor("Temperatura"), new Local(21, 23, 50), new Date(21 / 11 / 2018)));
         Room r1 = new Room("Cozinha", 1, 123, 2,2);
         r1.setRoomSensorList(sensorList);
         Room r2 = new Room("Jardim", 1, 123, 2,2);
@@ -43,7 +43,7 @@ public class RoomListTest {
     @Test
     public void seeIfDoesListOfRoomsContainRoomByName() {
         RoomList roomList = new RoomList();
-        SensorList sensorList = new SensorList(new Sensor("s1", new TypeSensor("Temperatura"), new Local(21, 23), new Date(21 / 11 / 2018)));
+        SensorList sensorList = new SensorList(new Sensor("s1", new TypeSensor("Temperatura"), new Local(21, 23, 50), new Date(21 / 11 / 2018)));
         Room r1 = new Room("Cozinha", 1, 123, 2,2);
         r1.setRoomSensorList(sensorList);
         Room r2 = new Room("Jardim", 1, 123, 2,2);
@@ -63,7 +63,7 @@ public class RoomListTest {
     @Test
     public void seeIfDoesListOfRoomsContainRoomByNameFalse() {
         RoomList roomList = new RoomList();
-        SensorList sensorList = new SensorList(new Sensor("s1", new TypeSensor("Temperatura"), new Local(21, 23), new Date(21 / 11 / 2018)));
+        SensorList sensorList = new SensorList(new Sensor("s1", new TypeSensor("Temperatura"), new Local(21, 23, 50), new Date(21 / 11 / 2018)));
         Room r1 = new Room("Cozinha", 1, 123, 2,2);
         r1.setRoomSensorList(sensorList);
         Room r2 = new Room("Jardim", 1, 123, 2,2);
@@ -83,7 +83,7 @@ public class RoomListTest {
     @Test
     public void seeIfAddRoomFails(){
         RoomList roomList = new RoomList();
-        SensorList sensorList = new SensorList(new Sensor("s1", new TypeSensor("Temperatura"), new Local(21, 23), new Date(21 / 11 / 2018)));
+        SensorList sensorList = new SensorList(new Sensor("s1", new TypeSensor("Temperatura"), new Local(21, 23, 50), new Date(21 / 11 / 2018)));
         Room r1 = new Room("Cozinha", 1, 123, 2,2);
         r1.setRoomSensorList(sensorList);
         Room r2 = new Room("Cozinha", 1, 123, 2,2);
@@ -99,7 +99,7 @@ public class RoomListTest {
     @Test
     public void seeIfAddRoomPasses(){
         RoomList roomList = new RoomList();
-        SensorList sensorList = new SensorList(new Sensor("s1", new TypeSensor("Temperatura"), new Local(21, 23), new Date(21 / 11 / 2018)));
+        SensorList sensorList = new SensorList(new Sensor("s1", new TypeSensor("Temperatura"), new Local(21, 23, 50), new Date(21 / 11 / 2018)));
         Room r1 = new Room("Cozinha", 1, 123, 2,2);
         r1.setRoomSensorList(sensorList);
         Room r2 = new Room("Jardim", 1, 123, 2,2);
