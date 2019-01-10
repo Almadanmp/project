dd6238c37827a853dbd831734e31d257a45767af
migue <1181718@isep.ipp.pt>
2019-01-10 16:32:15 +0000

    undo previous commit

--- src/main/java/pt/ipp/isep/dei/project/io/ui/EnergyGridSettingsUI.java
+++ src/main/java/pt/ipp/isep/dei/project/io/ui/EnergyGridSettingsUI.java
@@ -52,12 +52,11 @@ class EnergyGridSettingsUI {
                 case 4:
                     getInputRoom(house);
                     getInputEnergyGrid(house);
-                    if (mEnergyGrid != null && mRoom != null) {
-                        if (updateGridUS147(mEnergyGrid, mRoom)) {
-                            System.out.println("Room successfully added to the grid!");
-                        } else System.out.println("It wasn't possible to add the room. Please try again.");
-                        activeInput = false;
-                    }
+                    if (updateGridUS147(mEnergyGrid, mRoom)) {
+                        System.out.println("Room successfully added to the grid!");
+                    } else System.out.println("It wasn't possible to add the room. Please try again.");
+
+                    activeInput = false;
                     break;
                 case 5:
                     getInputEnergyGrid(house);
