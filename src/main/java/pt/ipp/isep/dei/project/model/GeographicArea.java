2ec57d5673fbef238ec588e8f9ae74260f8b91b0
danie <1181699@isep.ipp.pt>
2019-01-11 11:28:54 +0000

    GeographicArea Tried to fix seeifitscontained

--- src/main/java/pt/ipp/isep/dei/project/model/GeographicArea.java
+++ src/main/java/pt/ipp/isep/dei/project/model/GeographicArea.java
@@ -215,15 +215,15 @@ public class GeographicArea {
         return this.mLength;
     }
 
-    boolean isAreaContainedInAnotherArea(GeographicArea area1, GeographicArea area2) {
-        double latTopVert1 = area1.getLocal().getLatitude() + (area1.getWidth() / 2);
-        double longTopVert1 = area1.getLocal().getLongitude() - (area1.getLength() / 2);
-        double latBotVert1 = area1.getLocal().getLatitude() - (area1.getWidth() / 2);
-        double longBotVert1 = area1.getLocal().getLongitude() + (area1.getLength() / 2);
-        double latTopVert2 = area2.getLocal().getLatitude() + (area2.getWidth() / 2);
-        double longTopVert2 = area2.getLocal().getLongitude() - (area2.getLength() / 2);
-        double latBotVert2 = area2.getLocal().getLatitude() - (area2.getWidth() / 2);
-        double longBotVert2 = area2.getLocal().getLongitude() + (area2.getLength() / 2);
+    boolean isAreaContainedInAnotherArea(GeographicArea smallerAG, GeographicArea biggestAG) {
+        double latTopVert1 = smallerAG.getLocal().getLatitude() + (smallerAG.getWidth() / 2);
+        double longTopVert1 = smallerAG.getLocal().getLongitude() - (smallerAG.getLength() / 2);
+        double latBotVert1 = smallerAG.getLocal().getLatitude() - (smallerAG.getWidth() / 2);
+        double longBotVert1 = smallerAG.getLocal().getLongitude() + (smallerAG.getLength() / 2);
+        double latTopVert2 = biggestAG.getLocal().getLatitude() + (biggestAG.getWidth() / 2);
+        double longTopVert2 = biggestAG.getLocal().getLongitude() - (biggestAG.getLength() / 2);
+        double latBotVert2 = biggestAG.getLocal().getLatitude() - (biggestAG.getWidth() / 2);
+        double longBotVert2 = biggestAG.getLocal().getLongitude() + (biggestAG.getLength() / 2);
         return (latTopVert2 <= latTopVert1 && longTopVert2 >= longTopVert1 && latBotVert2 >= latBotVert1 && longBotVert2 <= longBotVert1);
     }
 
