e54ef1f67236e62e07b8a106e99ce5ebfc21a8e5
Cárina <carina.alas11@gmail.com>
2019-01-11 12:38:51 +0000

    Added Tests to "ReadingList" - updated method

--- src/main/java/pt/ipp/isep/dei/project/model/ReadingList.java
+++ src/main/java/pt/ipp/isep/dei/project/model/ReadingList.java
@@ -353,7 +353,7 @@ public class ReadingList {
         checkIfListValid(valueList);
         double highestValue = valueList.get(0);
         for (double value : valueList) {
-            if (value > highestValue) {
+            if (value >= highestValue) {
                 highestValue = value;
             }
         }
@@ -495,14 +495,13 @@ public class ReadingList {
         cal.set(Calendar.MINUTE, 59);
         cal.set(Calendar.SECOND, 59);
         cal.add(Calendar.DAY_OF_MONTH, -1);
-        cal.set(Calendar.MILLISECOND, 999);
         Date beginDay = cal.getTime();
 
         cal.setTime(dateGiven);
         cal.set(Calendar.SECOND, 0);
         cal.add(Calendar.DAY_OF_MONTH, +1);
-        cal.set(Calendar.HOUR_OF_DAY, 0);
         cal.set(Calendar.MINUTE, 0);
+        cal.set(Calendar.HOUR_OF_DAY, 0);
         Date endDay = cal.getTime();
 
         double maxValue = mReadings.get(0).getmValue();
