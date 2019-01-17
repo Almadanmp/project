0ef2841761c9ff793f57ac4be9f6c147e22d9cee
Andre Rua <1181689@isep.ipp.pt>
2019-01-17 13:02:35 +0000

    #53 Added javadocs

--- src/main/java/pt/ipp/isep/dei/project/model/EnergyGrid.java
+++ src/main/java/pt/ipp/isep/dei/project/model/EnergyGrid.java
@@ -20,12 +20,20 @@ public class EnergyGrid implements Metered {
     private PowerSourceList mListPowerSources;
     private String mStringSpacer = "---------------\n";
 
+    /**
+     * Empty constructor to use on UIs.
+     */
+
     public EnergyGrid() {
         this.mRoomList = new RoomList();
         this.mListPowerSources = new PowerSourceList();
         mNominalPower = 0;
     }
-
+    /**
+     * Builder EnergyGrid(), with all the parameters to define a grid.
+     * @param houseGridDesignation the name of the energy grid.
+     * @param maxContractedPower value of max nominal power a grid is able to sustain.
+     */
 
     public EnergyGrid(String houseGridDesignation, double maxContractedPower) {
         setName(houseGridDesignation);
@@ -33,20 +41,28 @@ public class EnergyGrid implements Metered {
         this.mRoomList = new RoomList();
         this.mListPowerSources = new PowerSourceList();
     }
-
+    /**
+     * Getter for name of the energy grid.
+     * @return returns attribute name of the energy grid.
+     */
 
     public String getName() {
         return mName;
     }
+
+    /**
+     * Getter for list of rooms that are contained in the energy grid.
+     * @return returns a list with all the rooms contained in this energy grid.
+     */
 
     public RoomList getListOfRooms() {
         return mRoomList;
     }
 
     /**
+     * Getter for list of devices that are contained in all the rooms of this energy grid.
      * @return a DeviceList containing all the devices in the energy grid list of rooms.
      */
-
     public DeviceList getDeviceListFromAllRooms() {
         DeviceList devices = new DeviceList();
         for (Room r : mRoomList.getRoomList()) {
@@ -57,6 +73,10 @@ public class EnergyGrid implements Metered {
         return devices;
     }
 
+    /**
+     * Getter for the total nominal power value in every room contained in the grid.
+     * @return a the total nominal power value in every room contained in the grid.
+     */
     public double getNominalPower() {
         double result = 0;
         for (Room r : mRoomList.getRoomList()) {
@@ -66,18 +86,35 @@ public class EnergyGrid implements Metered {
         return mNominalPower;
     }
 
+    /**
+     * Getter for the list of power sources connected to the energy grid.
+     * @return the list of power sources connected to the energy grid.
+     */
     PowerSourceList getListPowerSources() {
         return mListPowerSources;
     }
 
+    /**
+     * Setter for List of power sources connected to the energy grid.
+     * @param mListPowerSources List of power sources.
+     */
     void setListPowerSources(PowerSourceList mListPowerSources) {
         this.mListPowerSources = mListPowerSources;
     }
 
+    /**
+     * Setter for the list of rooms connected the energy grid.
+     * @param mListOfRooms List of rooms in the energy grid.
+     */
     public void setRoomList(RoomList mListOfRooms) {
         this.mRoomList = mListOfRooms;
     }
 
+    /**
+     * Setter for a power source to the energy grid.
+     * @param powerSource power source to be added to the energy grid.
+     * @return returns true case the power source to be added is already associated to the grid.
+     */
     public boolean addPowerSource(PowerSource powerSource) {
         if (!(mListPowerSources.getPowerSourceList().contains(powerSource))) {
             return mListPowerSources.getPowerSourceList().add(powerSource);
@@ -85,30 +122,59 @@ public class EnergyGrid implements Metered {
         return false;
     }
 
+    /**
+     * Setter for the value of nominal power in the energy grid.
+     * @param nominalPower value of nominal power in the energy grid.
+     */
     private void setNominalPower(double nominalPower) {
         this.mNominalPower = nominalPower;
     }
 
+    /**
+     * Setter for the name of the energy grid.
+     * @param mName name of the energy grid.
+     */
     public void setName(String mName) {
         this.mName = mName;
     }
 
+    /**
+     * Setter for a new room to be added to the energy grid.
+     * @param room Room to be added to the list of rooms in the energy grid.
+     * @return returns true if the room is actually added to the energy grid.
+     */
     public boolean addRoomToAnEnergyGrid(Room room) {
         return this.mRoomList.addRoom(room);
     }
 
+    /**
+     * Creates a string displaying the name of the grid and respective max nominal power.
+     * @return returns a string displaying the name of the grid and respective max nominal power.
+     */
     public String buildGridString() {
         return "Energy Grid: " + this.mName + ", Max Power: " + this.getNominalPower();
     }
 
+    /**
+     * Getter for the list of rooms related to the energy grid as an array of rooms.
+     * @return a list of rooms related to the energy grid.
+     */
     public List<Room> getRoomList() {
         return this.mRoomList.getRoomList();
     }
 
+    /**
+     * Getter for the list of devices related to the energy grid as an array od devices.
+     * @return a list of devices related to the energy grid.
+     */
     public List<Device> getDeviceList() {
         return this.mRoomList.getDeviceList();
     }
 
+    /**
+     * Creates a string displaying the name of the rooms and the devices connected to the energy grid.
+     * @return returns a string displaying the name of the rooms and the devices connected to the energy grid.
+     */
     public String buildRoomsAndDevicesString() {
         int counter = 0;
         StringBuilder result = new StringBuilder();
@@ -125,6 +191,11 @@ public class EnergyGrid implements Metered {
         return result.toString();
     }
 
+    /**
+     * Method to remove a room from the energy grid.
+     * @param room the room we want to remove from the energy grid.
+     * @return returns true if the room is successfully removed from the energy grid.
+     */
     public boolean removeRoom(Room room) {
         if (this.mRoomList.contains(room)) {
             this.mRoomList.removeRoom(room);
@@ -133,6 +204,10 @@ public class EnergyGrid implements Metered {
         return false;
     }
 
+    /**
+     * Creates a string displaying the name of the devices in the energy grid.
+     * @return returns a string displaying the name of the devices in the energy grid.
+     */
     public String buildDeviceListString() {
         int counter = 0;
         StringBuilder result = new StringBuilder();
@@ -143,16 +218,20 @@ public class EnergyGrid implements Metered {
         return result.toString();
     }
 
+    /**
+     * Creates a string displaying the rooms in the energy grid and the room's attributes.
+     * @return returns a string displaying the rooms in the energy grid and the room's attributes.
+     */
     public String buildRoomListString() {
         return this.mRoomList.buildRoomsString();
     }
 
 
     /**
-     * @param energyGrid - parameter which we will use to get all the devices present on all of its rooms.
+     * Creates a String with the device index, device type, device name and the room in which the device is contained.
+     * @param energyGrid energy grid that we want to see which devices will be printed.
      * @return a String with the device index, device type, device name and the room in which the device is contained.
      */
-
     public String buildListOfDeviceByTypeString(EnergyGrid energyGrid) {
         StringBuilder result = new StringBuilder(mStringSpacer);
         for (DeviceType d : DeviceType.values()) {
@@ -165,6 +244,12 @@ public class EnergyGrid implements Metered {
         return result.toString();
     }
 
+    /**
+     * Creates a string that displays the device type and in which room it is contained.
+     * @param r room where we want to see the devices.
+     * @param d type of the devices.
+     * @return a string that displays the device type and in which room it is contained.
+     */
     private String buildDeviceListInGridString(Room r, DeviceType d) {
         StringBuilder result = new StringBuilder();
         for (int x = 0; x < r.getDeviceList().size(); x++) {
