cd751a5872d56aba10a79f12a9788dda00a5dd07
Daniela <1181701@isep.ipp.pt>
2019-01-26 23:29:43 +0000

    adds TestUtils class This class has static strings to be used only on tests

---
+++ src/test/java/pt/ipp/isep/dei/project/TestUtils.java
@@ -0,0 +1,38 @@
+package pt.ipp.isep.dei.project;
+
+public class TestUtils {
+
+    /**
+     * Class with strings to use on tests.
+     */
+
+    //Devices
+    public static final String PATH_TO_FRIDGE = "pt.ipp.isep.dei.project.model.device.devicetypes.Fridge";
+    public static final String PATH_TO_WATERHEATER = "pt.ipp.isep.dei.project.model.device.devicetypes.WaterHeater";
+    public static final String PATH_TO_DISHWASHER = "pt.ipp.isep.dei.project.model.device.devicetypes.Dishwasher";
+    public static final String PATH_TO_WASHINGMACHINE = "pt.ipp.isep.dei.project.model.device.devicetypes.WashingMachine";
+    public static final String PATH_TO_LAMP = "pt.ipp.isep.dei.project.model.device.devicetypes.Lamp";
+
+    //Fridge attributes
+    public static final String F_FREEZER_CAPACITY = "freezerCapacity";
+    public static final String F_REFRIGERATOR_CAPACITY = "refrigeratorCapacity";
+    public static final String F_ANNUAL_CONSUMPTION = "annualEnergyConsumption";
+
+    //WaterHeater attributes
+    public static final String WH_VOLUME_OF_WATER = "volumeOfWater";
+    public static final String WH_HOT_WATER_TEMP = "hotWaterTemperature";
+    public static final String WH_OLD_WATER_TEMP = "coldWaterTemperature";
+    public static final String WH_PERFORMANCE_RATIO = "performanceRatio";
+    public static final String WH_VOLUME_OF_WATER_HEAT = "volumeOfWaterToHeat";
+
+    //Dishwasher attributes
+    public static final String DW_CAPACITY = "capacity";
+    public static final String DW_PROGRAM_LIST = "programList";
+
+    //WashingMachine attributes
+    public static final String WM_CAPACITY = "capacity";
+
+    //LAMP
+    public static final String FLUX = "LuminousFlux";
+
+}
