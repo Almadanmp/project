76158cdf82be14544aafb254118dd23226123db6
migue <1181718@isep.ipp.pt>
2019-01-31 12:23:23 +0000

    #103 - UML Sequence Diagram

--- src/test/java/pt/ipp/isep/dei/project/model/device/devicetypes/WaterheaterDTTest.java
+++ src/test/java/pt/ipp/isep/dei/project/model/device/devicetypes/WaterheaterDTTest.java
@@ -1,16 +1,31 @@
 package pt.ipp.isep.dei.project.model.device.devicetypes;
 
 import org.junit.jupiter.api.Test;
+import pt.ipp.isep.dei.project.model.device.Device;
+import pt.ipp.isep.dei.project.model.device.devicespecs.WashingMachineSpec;
+import pt.ipp.isep.dei.project.model.device.devicespecs.WaterHeaterSpec;
+
+import static org.testng.Assert.assertEquals;
 
 /**
  * device tests class.
  */
 
-public class WaterheaterDTTest {
+class WaterHeaterDTTest {
 
     @Test
-    public void createWaterheaterType() {
+    void createWaterHeaterType() {
         WaterHeaterDT dt = new WaterHeaterDT();
-        dt.createDeviceType();
+        Device result = dt.createDeviceType();
+        Device expectedResult = new Device(new WaterHeaterSpec());
+        assertEquals(result, expectedResult);
+    }
+
+    @Test
+    void getDeviceType() {
+        WaterHeaterDT dt = new WaterHeaterDT();
+        String result = dt.getDeviceType();
+        String expectedResult = "Water Heater";
+        assertEquals(result, expectedResult);
     }
 }
