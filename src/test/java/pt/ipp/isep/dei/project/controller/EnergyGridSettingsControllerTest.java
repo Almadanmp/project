344ab8198a9afe40b53fec974d83baad01eed831
Dark <1181722@isep.ipp.pt>
2019-02-19 10:34:54 +0000

    #132 Test for method added

--- src/test/java/pt/ipp/isep/dei/project/controller/EnergyGridSettingsControllerTest.java
+++ src/test/java/pt/ipp/isep/dei/project/controller/EnergyGridSettingsControllerTest.java
@@ -6,7 +6,16 @@ import org.junit.jupiter.api.Test;
 import org.testng.Assert;
 import pt.ipp.isep.dei.project.model.*;
 
+import pt.ipp.isep.dei.project.model.device.Device;
+import pt.ipp.isep.dei.project.model.device.DeviceList;
+import pt.ipp.isep.dei.project.model.device.Fridge;
+import pt.ipp.isep.dei.project.model.device.WashingMachine;
+import pt.ipp.isep.dei.project.model.device.devicespecs.FridgeSpec;
+import pt.ipp.isep.dei.project.model.device.devicetypes.DeviceType;
+
 import java.util.ArrayList;
+import java.util.List;
+
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
@@ -252,5 +261,40 @@ class EnergyGridSettingsControllerTest {
 
         assertEquals(actualResult1, powerSource1);
         assertEquals(actualResult2, powerSource2);
+    }
+
+    @Test
+    void testBuildListOfDevicesOrderedByTypeStringEmptyString() {
+        //Arrange
+        validHouse.addGrid(validGrid);
+        //Act
+        String expectedResult = "---------------\n" +
+                "---------------\n";
+        String actualResult = controller.buildListOfDevicesOrderedByTypeString(validGrid, validHouse);
+        //Arrange
+        assertEquals(expectedResult, actualResult);
+    }
+    @Test
+    void testBuildListOfDevicesOrderedByTypeStringWithDevices() {
+        //Arrange
+        List<String> deviceTypeString = new ArrayList<>();
+        deviceTypeString.add(PATH_TO_FRIDGE);
+        House house = new House("casa de praia", "Rua das Flores", "4512", "Porto", new Local(4, 5, 4), new GeographicArea("porto", new TypeArea("cidade"), 2, 3, new Local(4, 4, 100)), 60, 180, deviceTypeString);
+        Room room1EdC = new Room("B107", 1, 7, 11, 3.5);
+        EnergyGrid eg = new EnergyGrid("Main Energy Grid Edificio C", 333);
+        RoomList rl = new RoomList();
+        DeviceList deviceList = new DeviceList();
+        Device fridge = new Fridge(new FridgeSpec());
+        deviceList.addDevice(fridge);
+        room1EdC.setDeviceList(deviceList);
+        eg.setRoomList(rl);
+        rl.addRoom(room1EdC);
+        //Act
+        String expectedResult = "---------------\n" +
+                "Device type: Fridge | Device name: null | Nominal power: 0.0 | Room: B107 | \n" +
+                "---------------\n";
+        String actualResult = controller.buildListOfDevicesOrderedByTypeString(eg, house);
+        //Arrange
+        assertEquals(expectedResult, actualResult);
     }
 }
