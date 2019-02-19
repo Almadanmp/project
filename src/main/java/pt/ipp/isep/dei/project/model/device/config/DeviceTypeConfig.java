30621f1b9404c1340cc7ade770422c84afc437f3
Daniela <1181701@isep.ipp.pt>
2019-02-19 10:47:06 +0000

    REF #139 changes method to implementation with try with resources add javadocs

--- src/main/java/pt/ipp/isep/dei/project/model/device/config/DeviceTypeConfig.java
+++ src/main/java/pt/ipp/isep/dei/project/model/device/config/DeviceTypeConfig.java
@@ -8,25 +8,37 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Properties;
 
-/**
- * TODO - javadoc class and methods
- */
-
 public class DeviceTypeConfig {
 
-    public List<String> getDeviceTypeConfig() throws IOException {
-        return getDeviceTypeConfigFromSpecificFile("resources/devices.properties");
+    /**
+     *
+     * @param p - properties file
+     * @param key - key to the value we want to get
+     * @return - string "value" from the key
+     * @throws IOException - will throw an IOException if we cannot read the input key
+     */
+    public String getPropertyValueFromKey(Properties p, String key) throws IOException {
+        String result = p.getProperty(key);
+        if (result == null) {
+            throw new IOException("Could not read " + key + " property value.");
+        }
+        return result;
     }
 
+    /**
+     * Method to get a List of Strings with all the Paths contained on configuration file to the different device types
+     * available
+     * @param propFileName - the properties configuration file where the device types are
+     * @return - list of strings with device paths
+     * @throws IOException - will be thrown if there is any error with the configuration file
+     */
     public List<String> getDeviceTypeConfigFromSpecificFile(String propFileName) throws IOException {
         String allDevicesKey = "allDeviceTypes";
         Properties props = new Properties();
 
         List<String> deviceTypeConfig = new ArrayList<>();
-        InputStream input;
 
-        try {
-            input = new FileInputStream(propFileName);
+        try (InputStream input = new FileInputStream(propFileName)) {
             props.load(input);
             String deviceTypes = getPropertyValueFromKey(props, allDevicesKey);
             List<String> deviceTypeList = Arrays.asList(deviceTypes.split(","));
@@ -40,12 +52,16 @@ public class DeviceTypeConfig {
         return deviceTypeConfig;
     }
 
-    public String getPropertyValueFromKey(Properties p, String key) throws IOException {
-        String result = p.getProperty(key);
-        if (result == null) {
-            throw new IOException("Could not read " + key + " property value.");
-        }
-        return result;
+    /**
+     * Method to get a List of Strings with all the Paths contained on configuration file to the different device types
+     * available
+     * This method is separated from the above so we can test with different propfilenames (wrong etc).
+     * @return list of strings with device paths
+     * @throws IOException
+     */
+    public List<String> getDeviceTypeConfig() throws IOException {
+        return getDeviceTypeConfigFromSpecificFile("resources/devices.properties");
     }
+
 }
 
