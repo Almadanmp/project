8b4803be7d87bc31ee351a46b522a51e968fb56f
Dark <1181722@isep.ipp.pt>
2019-02-20 15:22:58 +0000

    REF #153 Fixed dTQuotient codesmell 2nd Try substituted < for <=

--- src/main/java/pt/ipp/isep/dei/project/model/device/WaterHeater.java
+++ src/main/java/pt/ipp/isep/dei/project/model/device/WaterHeater.java
@@ -123,7 +123,7 @@ public class WaterHeater implements Device, Metered {
         double coldWaterTemperature = (double) wHDeviceSpecs.getAttributeValue("Cold Water Temperature");
         double hotWaterTemperature = (double) wHDeviceSpecs.getAttributeValue("Hot Water Temperature");
         double result = hotWaterTemperature - coldWaterTemperature;
-        if (result < 0) {
+        if (result <= 0) {
             result = 0;
         }
         return result;
