cc1f7627f7adcfc59f055e4976197f01a2ef7d01
Dark <1181722@isep.ipp.pt>
2019-02-20 11:25:05 +0000

    REF #147 testGetSensorList reverted

--- src/test/java/pt/ipp/isep/dei/project/model/TypeSensorListTest.java
+++ src/test/java/pt/ipp/isep/dei/project/model/TypeSensorListTest.java
@@ -38,20 +38,20 @@ public class TypeSensorListTest {
         typeList2.add(typeSensor1);
         typeList3.add(typeSensor1);
         typeList3.add(typeSensor2);
-        //Act
         List<TypeSensor> expectedResult1 = new ArrayList<>();
         List<TypeSensor> expectedResult2 = new ArrayList<>();
         List<TypeSensor> expectedResult3 = new ArrayList<>();
         expectedResult2.add(typeSensor1);
         expectedResult3.add(typeSensor1);
         expectedResult3.add(typeSensor2);
+        //Act
         List<TypeSensor> actualResult1 = typeList1.getTypeSensorList();
         List<TypeSensor> actualResult2 = typeList2.getTypeSensorList();
         List<TypeSensor> actualResult3 = typeList3.getTypeSensorList();
         //Assert
-        assertEquals(expectedResult1,actualResult1);
-        assertEquals(expectedResult2,actualResult2);
-        assertEquals(expectedResult3,actualResult3);
+        assertEquals(actualResult1,expectedResult1);
+        assertEquals(actualResult2,expectedResult2);
+        assertEquals(actualResult3,expectedResult3);
     }
 
     @Test
