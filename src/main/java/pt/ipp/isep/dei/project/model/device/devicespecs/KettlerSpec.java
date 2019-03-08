262a5486e6ee39bb56562b42f9f088e15b5a246b
Teresa Varela Gomes <1181730@isep.ipp.pt>
2019-03-08 15:03:41 +0000

    New class Kettler New class Kettler Spec
    Resources:
    - Changed devices.properties file to include Kettler

---
+++ src/main/java/pt/ipp/isep/dei/project/model/device/devicespecs/KettlerSpec.java
@@ -0,0 +1,4 @@
+package pt.ipp.isep.dei.project.model.device.devicespecs;
+
+public class KettlerSpec {
+}
---
+++ src/main/java/pt/ipp/isep/dei/project/model/device/program/Kettler.java
@@ -0,0 +1,4 @@
+package pt.ipp.isep.dei.project.model.device.program;
+
+public class Kettler {
+}
