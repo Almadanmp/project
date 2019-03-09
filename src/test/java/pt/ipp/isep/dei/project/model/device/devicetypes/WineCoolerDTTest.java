63ae62f7865bd457dbba3142d1cafc6e0dcfc4f6
João Cachada <jmcs.cachada@gmail.com>
2019-03-09 18:13:43 +0000

    Refs #122
    Fixes WineCoolerDTTest class so it doesn't have repeated instantiations
    of testing artifacts.

--- src/test/java/pt/ipp/isep/dei/project/model/device/devicetypes/WineCoolerDTTest.java
+++ src/test/java/pt/ipp/isep/dei/project/model/device/devicetypes/WineCoolerDTTest.java
@@ -1,27 +1,50 @@
 package pt.ipp.isep.dei.project.model.device.devicetypes;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import pt.ipp.isep.dei.project.model.device.Device;
-import pt.ipp.isep.dei.project.model.device.devicespecs.WineCoolerSpec;
 import pt.ipp.isep.dei.project.model.device.WineCooler;
+import pt.ipp.isep.dei.project.model.device.devicespecs.WineCoolerSpec;
 
 import static org.testng.Assert.*;
 
-public class WineCoolerDTTest {
+class WineCoolerDTTest {
+    // Common testing artifacts for tests in this class.
+    private WineCoolerDT coolerType;
+
+    @BeforeEach
+    void arrangeArtifacts(){
+        coolerType = new WineCoolerDT();
+    }
 
     @Test
-    void createWineCoolerType() {
-        WineCoolerDT dt = new WineCoolerDT();
-        Device result = dt.createDevice();
+    void seeIfCreateDeviceWorks() {
+        // Arrange
+
         Device expectedResult = new WineCooler(new WineCoolerSpec());
+
+        // Act
+
+        Device result = coolerType.createDevice();
+
+        // Assert
+
         assertEquals(result, expectedResult);
     }
 
     @Test
-    void getDeviceType() {
-        WineCoolerDT dt = new WineCoolerDT();
-        String result = dt.getDeviceType();
+    void seeIfGetDeviceTypeWorks() {
+        // Arrange
+
         String expectedResult = "WineCooler";
+
+        // Act
+
+        String result = coolerType.getDeviceType();
+
+        // Assert
+
         assertEquals(result, expectedResult);
     }
+
 }
