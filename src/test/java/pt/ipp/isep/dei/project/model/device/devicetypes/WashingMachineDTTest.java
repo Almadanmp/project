9e6898b99f6a4194c37a20fda06f8516b7b96bb1
João Cachada <jmcs.cachada@gmail.com>
2019-03-09 18:11:41 +0000

    Refs #122
    Fixes WashingMachineDTTest class so it doesn't have repeated instantiations
    of testing artifacts.

--- src/test/java/pt/ipp/isep/dei/project/model/device/devicetypes/WashingMachineDTTest.java
+++ src/test/java/pt/ipp/isep/dei/project/model/device/devicetypes/WashingMachineDTTest.java
@@ -1,5 +1,6 @@
 package pt.ipp.isep.dei.project.model.device.devicetypes;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import pt.ipp.isep.dei.project.model.device.Device;
 import pt.ipp.isep.dei.project.model.device.WashingMachine;
@@ -13,19 +14,42 @@ import static org.testng.Assert.assertEquals;
 
 class WashingMachineDTTest {
 
+    // Common testing artifacts for tests in this class.
+    private WashingMachineDT washingMachineType;
+
+    @BeforeEach
+    void arrangeArtifacts() {
+        washingMachineType = new WashingMachineDT();
+    }
+
     @Test
-    void createWashingMachineType() {
-        WashingMachineDT dt = new WashingMachineDT();
-        Device result = dt.createDevice();
+    void seeIfCreateDeviceWorks() {
+        // Arrange
+
         Device expectedResult = new WashingMachine(new WashingMachineSpec());
+
+        // Act
+
+        Device result = washingMachineType.createDevice();
+
+        // Assert
+
         assertEquals(result, expectedResult);
     }
 
     @Test
-    void getDeviceType() {
-        WashingMachineDT dt = new WashingMachineDT();
-        String result = dt.getDeviceType();
+    void seeIfGetDeviceTypeWorks() {
+        // Arrange
+
         String expectedResult = "Washing Machine";
+
+        // Act
+
+        String result = washingMachineType.getDeviceType();
+
+        // Assert
+
         assertEquals(result, expectedResult);
     }
+
 }
