f363cc6eb389464810e7a5e2b7d8fbd9f2f0b378
João Cachada <jmcs.cachada@gmail.com>
2019-03-09 18:08:45 +0000

    Refs #122
    Fixes StoveDTTest class so it doesn't have repeated instantiations
    of testing artifacts.

--- src/test/java/pt/ipp/isep/dei/project/model/device/devicetypes/StoveDTTest.java
+++ src/test/java/pt/ipp/isep/dei/project/model/device/devicetypes/StoveDTTest.java
@@ -1,5 +1,6 @@
 package pt.ipp.isep.dei.project.model.device.devicetypes;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import pt.ipp.isep.dei.project.model.device.Device;
 import pt.ipp.isep.dei.project.model.device.Stove;
@@ -7,21 +8,42 @@ import pt.ipp.isep.dei.project.model.device.devicespecs.StoveSpec;
 
 import static org.testng.Assert.*;
 
-public class StoveDTTest {
+class StoveDTTest {
+    // Common testing artifacts for tests in this class.
+    private StoveDT stoveType;
+
+    @BeforeEach
+    void arrangeArtifacts(){
+        stoveType = new StoveDT();
+    }
 
     @Test
-    void ensureWeCreateStoveType() {
-        StoveDT dt = new StoveDT();
-        Device result = dt.createDevice();
+    void seeIfCreateDeviceWorks() {
+        // Arrange
+
         Device expectedResult = new Stove(new StoveSpec());
+
+        // Act
+
+        Device result = stoveType.createDevice();
+
+        // Assert
+
         assertEquals(result, expectedResult);
     }
 
     @Test
-    void ensureWeGetDeviceType() {
-        StoveDT dt = new StoveDT();
-        String result = dt.getDeviceType();
+    void seeIfGetDeviceTypeWorks() {
+        // Arrange
+
         String expectedResult = "Stove";
+
+        // Act
+
+        String result = stoveType.getDeviceType();
+
+        // Assert
+
         assertEquals(result, expectedResult);
     }
 
