f351171b7c7ea9464b608269b2e2e0bb4c9b3986
João Cachada <jmcs.cachada@gmail.com>
2019-03-09 18:02:08 +0000

    Refs #122
    Fixes FreezerDTTest class so it doesn't have repeated instantiations
    of testing artifacts.

--- src/test/java/pt/ipp/isep/dei/project/model/device/devicetypes/FreezerDTTest.java
+++ src/test/java/pt/ipp/isep/dei/project/model/device/devicetypes/FreezerDTTest.java
@@ -1,5 +1,6 @@
 package pt.ipp.isep.dei.project.model.device.devicetypes;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import pt.ipp.isep.dei.project.model.device.Device;
 import pt.ipp.isep.dei.project.model.device.Freezer;
@@ -7,20 +8,43 @@ import pt.ipp.isep.dei.project.model.device.devicespecs.FreezerSpec;
 
 import static org.testng.Assert.*;
 
-public class FreezerDTTest {
+class FreezerDTTest {
+    // Common testing artifacts for tests in this class.
+    private FreezerDT freezerType;
+
+    @BeforeEach
+    void arrangeArtifacts(){
+        freezerType = new FreezerDT();
+    }
+
     @Test
-    void createFreezerType() {
-       FreezerDT dt = new FreezerDT();
-        Device result = dt.createDevice();
+    void seeIfCreateDeviceWorks() {
+        // Arrange
+
         Device expectedResult = new Freezer(new FreezerSpec());
+
+        // Act
+
+        Device result = freezerType.createDevice();
+
+        // Assert
+
         assertEquals(result, expectedResult);
     }
 
     @Test
-    void getDeviceType() {
-        FreezerDT dt = new FreezerDT();
-        String result = dt.getDeviceType();
+    void seeIfGetDeviceTypeWorks() {
+        // Arrange
+
         String expectedResult = "Freezer";
+
+        // Act
+
+        String result = freezerType.getDeviceType();
+
+        // Assert
+
         assertEquals(result, expectedResult);
     }
+
 }
