0031c43b25b96001cd863fbc5ca6dad0a876fee1
João Cachada <jmcs.cachada@gmail.com>
2019-03-09 18:05:18 +0000

    Refs #122
    Fixes MicrowaveOvenDTTest class so it doesn't have repeated instantiations
    of testing artifacts.

--- src/test/java/pt/ipp/isep/dei/project/model/device/devicetypes/MicrowaveOvenDTTest.java
+++ src/test/java/pt/ipp/isep/dei/project/model/device/devicetypes/MicrowaveOvenDTTest.java
@@ -1,5 +1,6 @@
 package pt.ipp.isep.dei.project.model.device.devicetypes;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import pt.ipp.isep.dei.project.model.device.Device;
 import pt.ipp.isep.dei.project.model.device.MicrowaveOven;
@@ -7,21 +8,42 @@ import pt.ipp.isep.dei.project.model.device.devicespecs.MicrowaveOvenSpec;
 
 import static org.testng.Assert.*;
 
-public class MicrowaveOvenDTTest {
+class MicrowaveOvenDTTest {
+    // Common testing artifacts for tests in this class.
+    private MicrowaveOvenDT ovenType;
+
+    @BeforeEach
+    void arrangeArtifacts(){
+        ovenType = new MicrowaveOvenDT();
+    }
 
     @Test
-    void createMicrowaveOvenType() {
-        MicrowaveOvenDT dt = new MicrowaveOvenDT();
-        Device result = dt.createDevice();
+    void seeIfCreateDeviceWorks() {
+        // Arrange
+
         Device expectedResult = new MicrowaveOven(new MicrowaveOvenSpec());
+
+        // Act
+
+        Device result = ovenType.createDevice();
+
+        // Assert
+
         assertEquals(result, expectedResult);
     }
 
     @Test
-    void getDeviceType() {
-        MicrowaveOvenDT dt = new MicrowaveOvenDT();
-        String result = dt.getDeviceType();
+    void seeIfGetDeviceTypeWorks() {
+        // Arrange
+
         String expectedResult = "Microwave Oven";
+
+        // Act
+
+        String result = ovenType.getDeviceType();
+
+        // Assert
+
         assertEquals(result, expectedResult);
     }
 
