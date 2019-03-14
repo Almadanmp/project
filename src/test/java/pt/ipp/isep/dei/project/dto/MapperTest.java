52596cdf85b0e2ba5a1240d5fd0c25e90e63d930
Teresa Varela Gomes <1181730@isep.ipp.pt>
2019-03-14 23:35:38 +0000

    class Mapper Test: - test for UpdateHouseRoom

--- src/test/java/pt/ipp/isep/dei/project/dto/MapperTest.java
+++ src/test/java/pt/ipp/isep/dei/project/dto/MapperTest.java
@@ -3,6 +3,7 @@ package pt.ipp.isep.dei.project.dto;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import pt.ipp.isep.dei.project.model.*;
+import pt.ipp.isep.dei.project.model.device.DeviceList;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
