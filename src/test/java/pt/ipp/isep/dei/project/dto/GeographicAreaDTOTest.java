482107b9ac8689c634b1912b15db2036ae530da9
1181716 <1181716@isep.ipp.pt>
2019-03-14 23:03:21 +0000

    ref #171 tests added

--- src/test/java/pt/ipp/isep/dei/project/dto/GeographicAreaDTOTest.java
+++ src/test/java/pt/ipp/isep/dei/project/dto/GeographicAreaDTOTest.java
@@ -26,7 +26,9 @@ class GeographicAreaDTOTest {
         GeographicAreaDTO geographicAreaDTO4 = new GeographicAreaDTO();
         GeographicAreaDTO geographicAreaDTO5 = new GeographicAreaDTO();
         GeographicAreaDTO geographicAreaDTO6 = new GeographicAreaDTO();
-
+       GeographicAreaDTO geographicAreaDTO7 = new GeographicAreaDTO();
+       GeographicAreaDTO geographicAreaDTO8 = new GeographicAreaDTO();
+       GeographicAreaDTO geographicAreaDTO9 = new GeographicAreaDTO();
 
         geographicAreaDTO1.setDescription("geoAreaDTO1");
         geographicAreaDTO2.setDescription("geoAreaDTO2");
@@ -34,6 +36,9 @@ class GeographicAreaDTOTest {
         geographicAreaDTO4.setDescription("geoAreaDTO4");
         geographicAreaDTO5.setDescription("geoAreaDTO5");
         geographicAreaDTO6.setDescription("geoAreaDTO1");
+       geographicAreaDTO7.setDescription("geoAreaDTO1");
+       geographicAreaDTO8.setDescription("geoAreaDTO1");
+       geographicAreaDTO9.setDescription("geoAreaDTO1");
 
         geographicAreaDTO1.setId("01");
         geographicAreaDTO2.setId("02");
@@ -41,6 +46,9 @@ class GeographicAreaDTOTest {
         geographicAreaDTO4.setId("01");
         geographicAreaDTO5.setId("01");
         geographicAreaDTO6.setId("01");
+       geographicAreaDTO7.setId("01");
+       geographicAreaDTO8.setId("01");
+       geographicAreaDTO9.setId("01");
 
         geographicAreaDTO1.setAltitude(1);
         geographicAreaDTO2.setAltitude(2);
@@ -48,6 +56,9 @@ class GeographicAreaDTOTest {
         geographicAreaDTO4.setAltitude(1);
         geographicAreaDTO5.setAltitude(4);
         geographicAreaDTO6.setAltitude(1);
+       geographicAreaDTO7.setAltitude(1);
+       geographicAreaDTO8.setAltitude(1);
+       geographicAreaDTO9.setAltitude(1);
 
         geographicAreaDTO1.setLongitude(1);
         geographicAreaDTO2.setLongitude(2);
@@ -55,13 +66,19 @@ class GeographicAreaDTOTest {
         geographicAreaDTO4.setLongitude(1);
         geographicAreaDTO5.setLongitude(1);
         geographicAreaDTO6.setLongitude(1);
+       geographicAreaDTO7.setLongitude(5);
+       geographicAreaDTO8.setLongitude(1);
+       geographicAreaDTO9.setLongitude(1);
 
         geographicAreaDTO1.setLatitude(1);
         geographicAreaDTO2.setLatitude(2);
-        geographicAreaDTO3.setLatitude(5);
+        geographicAreaDTO3.setLatitude(1);
         geographicAreaDTO4.setLatitude(1);
         geographicAreaDTO5.setLatitude(1);
         geographicAreaDTO6.setLatitude(1);
+       geographicAreaDTO7.setLatitude(1);
+       geographicAreaDTO8.setLatitude(5);
+       geographicAreaDTO9.setLatitude(1);
 
         geographicAreaDTO1.setTypeArea("City");
         geographicAreaDTO2.setTypeArea("Town");
@@ -69,22 +86,31 @@ class GeographicAreaDTOTest {
         geographicAreaDTO4.setTypeArea("Country");
         geographicAreaDTO5.setTypeArea("City");
         geographicAreaDTO6.setTypeArea("City");
+       geographicAreaDTO7.setTypeArea("City");
+       geographicAreaDTO8.setTypeArea("City");
+       geographicAreaDTO9.setTypeArea("City");
 
         geographicAreaDTO1.setSensorDTOList(dtoList);
         geographicAreaDTO2.setSensorDTOList(dtoListEmpty);
         geographicAreaDTO3.setSensorDTOList(dtoList);
-        geographicAreaDTO4.setSensorDTOList(dtoListEmpty);
-        geographicAreaDTO5.setSensorDTOList(dtoListEmpty);
+        geographicAreaDTO4.setSensorDTOList(dtoList);
+        geographicAreaDTO5.setSensorDTOList(dtoList);
         geographicAreaDTO6.setSensorDTOList(dtoList);
+       geographicAreaDTO7.setSensorDTOList(dtoList);
+       geographicAreaDTO8.setSensorDTOList(dtoList);
+       geographicAreaDTO9.setSensorDTOList(dtoListEmpty);
 
         //Assert
 
-        assertEquals(geographicAreaDTO1, geographicAreaDTO1);
-        assertEquals(geographicAreaDTO1, geographicAreaDTO6);
-        assertNotEquals(geographicAreaDTO1, geographicAreaDTO2);
-        assertNotEquals(geographicAreaDTO1, geographicAreaDTO3);
-        assertNotEquals(geographicAreaDTO1, geographicAreaDTO4);
-        assertNotEquals(geographicAreaDTO1, geographicAreaDTO5);
+        assertEquals(geographicAreaDTO1, geographicAreaDTO1); // same object
+        assertEquals(geographicAreaDTO1, geographicAreaDTO6); // different object
+        assertNotEquals(geographicAreaDTO1, geographicAreaDTO2); //everything different
+        assertNotEquals(geographicAreaDTO1, geographicAreaDTO3); // id different
+        assertNotEquals(geographicAreaDTO1, geographicAreaDTO4); // type area
+        assertEquals(geographicAreaDTO1, geographicAreaDTO5); //altitude
+       assertNotEquals(geographicAreaDTO1,geographicAreaDTO7); //longitude
+       assertNotEquals(geographicAreaDTO1,geographicAreaDTO8); //latitude
+       assertNotEquals(geographicAreaDTO1,geographicAreaDTO9); //list
         assertNotEquals(geographicAreaDTO1, 3D);
     }
 
