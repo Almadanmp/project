47f72d5f4f06eb29511d63346bb5b5cb364f11dc
JCachada <jmcs.cachada@gmail.com>
2019-03-14 17:07:19 +0000

    Refs #171
    Fixes RoomDTO names, adds javadocs and cleans up code smells.

--- src/main/java/pt/ipp/isep/dei/project/dto/RoomDTO.java
+++ src/main/java/pt/ipp/isep/dei/project/dto/RoomDTO.java
@@ -8,74 +8,154 @@ import java.util.UUID;
 
 public class RoomDTO {
 
-    private String roomName;
-    private int houseFloor;
-    private double roomWidth;
-    private double roomLength;
-    private double roomHeight;
-    private SensorList roomSensorList;
+    private String name;
+    private int floor;
+    private double width;
+    private double length;
+    private double height;
+    private SensorList sensorList;
     private DeviceList deviceList;
     private UUID id;
 
-    public SensorList getRoomSensorList() {
-        return roomSensorList;
+    /**
+     * Method that retrieves the DTO's sensor list.
+     * @return is the DTO's sensorList.
+     */
+
+    public SensorList getSensorList() {
+        return sensorList;
     }
 
-    public void setRoomSensorList(SensorList roomSensorList) {
-        this.roomSensorList = roomSensorList;
+    /**
+     * Method that stores a particular sensorList in a DTO.
+     * @param sensorList is the list we want to store in the DTO.
+     */
+
+    public void setSensorList(SensorList sensorList) {
+        this.sensorList = sensorList;
     }
+
+    /**
+     * Method that retrieves the DTO's Device List.
+     * @return is the DTO's device list.
+     */
 
     public DeviceList getDeviceList() {
         return deviceList;
     }
 
-    public void setDeviceList(DeviceList deviceList) {
+    /**
+     * Method that stores a particular deviceList in the DTO.
+     * @param deviceList is the device list we want to store in the DTO.
+     */
+
+    void setDeviceList(DeviceList deviceList) {
         this.deviceList = deviceList;
     }
 
-    public String getRoomName() {
-        return roomName;
+    /**
+     * Method that retrieves the DTO's name.
+     * @return is the name of the DTO.
+     */
+
+    public String getName() {
+        return name;
     }
 
-    public void setRoomName(String roomName) {
-        this.roomName = roomName;
+    /**
+     * Method that stores a particular string as the DTO's name.
+     * @param name is the name we want to store.
+     */
+
+    public void setName(String name) {
+        this.name = name;
     }
 
-    public int getHouseFloor() {
-        return houseFloor;
+    /**
+     * Method that retrieves the floor of the DTO.
+     * @return is the floor.
+     */
+
+    public int getFloor() {
+        return floor;
     }
 
-    public void setHouseFloor(int houseFloor) {
-        this.houseFloor = houseFloor;
+    /**
+     * Method that stores an int as the DTO's floor.
+     * @param floor is the int we want to store.
+     */
+
+    public void setFloor(int floor) {
+        this.floor = floor;
     }
 
-    public double getRoomWidth() {
-        return roomWidth;
+    /**
+     * Method that retrieves the width of the DTO.
+     * @return is the DTO's width.
+     */
+
+    public double getWidth() {
+        return width;
     }
 
-    public void setRoomWidth(double roomWidth) {
-        this.roomWidth = roomWidth;
+    /**
+     * Method that stores a double as the DTO's width.
+     * @param width is the double we want to store.
+     */
+
+    public void setWidth(double width) {
+        this.width = width;
     }
 
-    public double getRoomLength() {
-        return roomLength;
+    /**
+     * Method that retrieves the length of the DTO.
+     * @return is the length of the DTO.
+     */
+
+    public double getLength() {
+        return length;
     }
 
-    public void setRoomLength(double roomLength) {
-        this.roomLength = roomLength;
+    /**
+     * Method that stores a double as the DTO's length.
+     * @param length is the double we want to store.
+     */
+
+    public void setLength(double length) {
+        this.length = length;
     }
 
-    public double getRoomHeight() {
-        return roomHeight;
+    /**
+     * Method that retrieves the height of the DTO.
+     * @return is the DTO's height.
+     */
+
+    public double getHeight() {
+        return height;
     }
 
-    public void setRoomHeight(double roomHeight) {
-        this.roomHeight = roomHeight;
+    /**
+     * Method that stores a double as the DTO's height.
+     * @param height is the height we want to store.
+     */
+
+    public void setHeight(double height) {
+        this.height = height;
     }
+
+    /**
+     * Method that retrieves the DTO's unique ID. This is useful to match it with a model object.
+     * @return is a UUID. UUID is a java class.
+     */
 
     public UUID getId() {
         return id;
     }
+
+    /**
+     * Method that stores a UUID as the object's UUID. This is useful to match the DTO with a model object.
+     * @param id is the UUID we want to store.
+     */
 
     public void setId(UUID id) {
         this.id = id;
