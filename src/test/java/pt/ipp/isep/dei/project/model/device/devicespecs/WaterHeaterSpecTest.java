b613c088342715380b82aee3de985e06b52b366b
Dark <1181722@isep.ipp.pt>
2019-03-14 16:29:19 +0000

    REF #171 trying for conditions

--- src/test/java/pt/ipp/isep/dei/project/model/device/devicespecs/WaterHeaterSpecTest.java
+++ src/test/java/pt/ipp/isep/dei/project/model/device/devicespecs/WaterHeaterSpecTest.java
@@ -279,7 +279,7 @@ class WaterHeaterSpecTest {
 
         assertEquals("L", validHeaterSpec.getAttributeUnit("Volume Of Water"));
         assertEquals("ºC", validHeaterSpec.getAttributeUnit("Hot Water Temperature"));
-        assertEquals("shit", validHeaterSpec.getAttributeUnit("Performance Ratio"));
+        assertEquals("", validHeaterSpec.getAttributeUnit("Performance Ratio"));
         assertEquals("ºC", validHeaterSpec.getAttributeUnit("Cold Water Temperature"));
         assertEquals("L", validHeaterSpec.getAttributeUnit("Volume Of Water To Heat"));
 
