654d84110f856a34ec64fb27b948f2e830076824
João Cachada <jmcs.cachada@gmail.com>
2019-03-28 11:47:29 +0000

    Refs #180.
    Fixes bug in SensorService by adding optional .isPresent check before
    findByID()

--- src/main/java/pt/ipp/isep/dei/project/Services/SensorService.java
+++ src/main/java/pt/ipp/isep/dei/project/Services/SensorService.java
@@ -19,14 +19,26 @@ public class SensorService {
     @Autowired
     private ReadingRepository readingRepository;
 
+    /**
+     * Method to add a reading to a matching sensor contained in the repository. The sensor is found by its ID.
+     * @param sensorList is the sensorList we want to add a reading to.
+     * @param sensorID is the ID of the sensor we want to add a reading to.
+     * @param readingValue is the value of the reading we want to add.
+     * @param readingDate is the date of the reading we want to add.
+     * @return is true if the reading is added (if there is a sensor with an ID that matches the given ID), false
+     * if there is no sensor with that ID.
+     */
 
     public boolean addReadingToMatchingSensor(SensorList sensorList, String sensorID, Double readingValue, Date readingDate) {
         sensorList.addReadingToMatchingSensor(sensorID, readingValue, readingDate);
-        Sensor sensor = sensorRepository.findById(sensorID).get();
-        Reading reading = new Reading(readingValue, readingDate);
-        reading.setReadingList(sensor.getReadingList());
-        readingRepository.save(reading);
-        return true;
+        if (sensorRepository.findById(sensorID).isPresent()) {
+            Sensor sensor = sensorRepository.findById(sensorID).get();
+            Reading reading = new Reading(readingValue, readingDate);
+            reading.setReadingList(sensor.getReadingList());
+            readingRepository.save(reading);
+            return true;
+        }
+        return false;
     }
 
 }
