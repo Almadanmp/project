c0c36b4bbbbcefdbb92dd557fed4c7e00ae632e8
danie <1181699@isep.ipp.pt>
2019-04-03 12:22:56 +0100

    REF #185
    - Added Rainfall Units javadocs
    - Added Adapter javadocs

--- src/main/java/pt/ipp/isep/dei/project/services/units/Adapter.java
+++ src/main/java/pt/ipp/isep/dei/project/services/units/Adapter.java
@@ -5,10 +5,26 @@ import java.io.IOException;
 public class Adapter {
 
 
+    /**
+     * This method is used to get a value in a previously defined unit and converts it into the system default unit.
+     *
+     * @param valueToConvert refers to the unit value.
+     * @param unitToConvert is an instance of the Unit class we want to convert.
+     * @return the value converted in the system default unit.
+     * @throws IOException in case the properties file is missing or doesn't have the property.
+     */
     public double convertToSystemDefault(double valueToConvert, Unit unitToConvert) throws IOException {
         return UnitHelper.convertToSystemDefault(valueToConvert,unitToConvert);
 }
 
+    /**
+     * This method is used to get a value in a previously defined unit and converts it into the user default unit.
+     *
+     * @param valueToConvert refers to the unit value.
+     * @param unitToConvert is an instance of the Unit class we want to convert.
+     * @return the value converted in the user default unit.
+     * @throws IOException in case the properties file is missing or doesn't have the property.
+     */
     public double convertToUserDefault(double valueToConvert, Unit unitToConvert) throws IOException {
         return UnitHelper.convertToUserDefault(valueToConvert,unitToConvert);
     }
--- src/main/java/pt/ipp/isep/dei/project/services/units/LiterPerSquareMeter.java
+++ src/main/java/pt/ipp/isep/dei/project/services/units/LiterPerSquareMeter.java
@@ -7,24 +7,53 @@ import static pt.ipp.isep.dei.project.services.units.UnitHelper.*;
 public class LiterPerSquareMeter implements RainfallUnit {
 
 
+    /**
+     * This method checks the application default rainfall unit and then converts a LiterPerSquareMeter value into the default one.
+     *
+     * @param valueToConvert refers to the rainfall value.
+     * @return value converted into the default Unit.
+     * @throws IOException in case the properties file is missing or doesn't have the property.
+     */
     public double toApplicationDefault(double valueToConvert) throws IOException {
         String defaultUnit = getApplicationRainfallDefault();
         return toDefaultRainfallUnit(defaultUnit, valueToConvert, this);
     }
 
+    /**
+     * This method checks the user default rainfall unit and then converts a LiterPerSquareMeter value into the default one.
+     *
+     * @param valueToConvert refers to the rainfall value.
+     * @return value converted into the default Unit.
+     * @throws IOException in case the properties file is missing or doesn't have the property.
+     */
     public double toUserDefault(double valueToConvert) throws IOException {
         String defaultUnit = getUserRainfallDefault();
         return toDefaultRainfallUnit(defaultUnit, valueToConvert, this);
     }
 
+    /**
+     * This method converts the rainfall value into Millimeter.
+     *
+     * @param volume refers to the rainfall in LiterPerSquareMeter.
+     * @return rainfall value in Millimeter.
+     */
     public double toMillimeter(double volume) {
         return volume;
     }
 
+    /**
+     * This method converts the rainfall value into LiterPerSquareMeter.
+     *
+     * @param volume refers to the rainfall in LiterPerSquareMeter.
+     * @return rainfall value in LiterPerSquareMeter.
+     */
     public double toLiterPerSquareMeter(double volume) {
         return volume;
     }
 
+    /**
+     * @return the rainfall unit type as a string
+     */
     public String buildString() {
         return "LiterPerSquareMeter";
     }
