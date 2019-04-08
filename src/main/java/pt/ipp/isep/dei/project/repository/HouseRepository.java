dfffcc76a13ba70ba28f19502a10482cae4c54e5
Daniela Carvalho <1181701@isep.ipp.pt>
2019-04-08 12:19:21 +0100

    REF#210 REF#197 creates houseService with method to save house on repository

--- src/main/java/pt/ipp/isep/dei/project/repository/HouseRepository.java
+++ src/main/java/pt/ipp/isep/dei/project/repository/HouseRepository.java
@@ -2,9 +2,12 @@ package pt.ipp.isep.dei.project.repository;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-import pt.ipp.isep.dei.project.model.AreaType;
+import pt.ipp.isep.dei.project.model.Address;
+import pt.ipp.isep.dei.project.model.House;
 
 @Repository
-public interface HouseRepository extends CrudRepository<AreaType, String> {
+public interface HouseRepository extends CrudRepository<House, Long> {
+
+    House findByAddress(Address address);
 
 }
---
+++ src/main/java/pt/ipp/isep/dei/project/services/HouseService.java
@@ -0,0 +1,29 @@
+package pt.ipp.isep.dei.project.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import pt.ipp.isep.dei.project.model.House;
+import pt.ipp.isep.dei.project.repository.HouseRepository;
+
+@Service
+public class HouseService {
+
+    @Autowired
+    private HouseRepository houseRepository;
+
+    public HouseService(HouseRepository houseRepository) {
+        this.houseRepository = houseRepository;
+    }
+
+
+    public void saveHouse(House house) {
+        House house2 = houseRepository.findByAddress(house.getAddress());
+        if (house2 != null) {
+            houseRepository.delete(house);
+            houseRepository.save(house);
+
+        }
+        houseRepository.save(house);
+    }
+}
+
