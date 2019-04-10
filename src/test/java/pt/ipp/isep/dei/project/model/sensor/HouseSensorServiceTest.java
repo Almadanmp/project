c873430268349113d1004204754b02e5fe728ab8
Andre Rua <1181689@isep.ipp.pt>
2019-04-10 01:17:16 +0100

    Ref #219 #197 Adds tests to HouseSensor class

---
+++ src/test/java/pt/ipp/isep/dei/project/model/sensor/HouseSensorServiceTest.java
@@ -0,0 +1,10 @@
+package pt.ipp.isep.dei.project.model.sensor;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * House Sensor Service tests class.
+ */
+class HouseSensorServiceTest {
+
+}
