e45f9bdd569c5e4d5ee9f356039144114b8e46ec
João Cachada <jmcs.cachada@gmail.com>
2019-04-26 12:14:24 +0100

    Properly closes handlers of sensorTypeLog.
    ... Again.

--- src/main/java/pt/ipp/isep/dei/project/model/geographicarea/GeographicAreaService.java
+++ src/main/java/pt/ipp/isep/dei/project/model/geographicarea/GeographicAreaService.java
@@ -212,6 +212,7 @@ public class GeographicAreaService {
             LogUtils.closeHandlers(logger);
             return null;
         } else {
+            LogUtils.closeHandlers(logger);
             return value.orElseGet(() -> new SensorType(name, unit));
         }
     }
