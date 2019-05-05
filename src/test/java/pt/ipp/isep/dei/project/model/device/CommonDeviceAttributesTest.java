7cfdb0feb1063926ea32b304550aa4668c744a92
Teresa Varela Gomes <1181730@isep.ipp.pt>
2019-05-05 16:35:57 +0100

    REF #319
    CommonDeviceAttributes
    - bug

---
+++ src/test/java/pt/ipp/isep/dei/project/model/device/CommonDeviceAttributesTest.java
@@ -0,0 +1,28 @@
+package pt.ipp.isep.dei.project.model.device;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CommonDeviceAttributesTest {
+
+    @Test
+    void seeIfBuildStringWorks() {
+        // Arrange
+
+        CommonDeviceAttributes commonDeviceAttributes = new CommonDeviceAttributes();
+        commonDeviceAttributes.setName("Fridge");
+        commonDeviceAttributes.setNominalPower(20D);
+
+        String expectedResult = "The device Name is Fridge, and its NominalPower is 20.0 kW.\n";
+
+        // Act
+
+        String actualResult = commonDeviceAttributes.buildString();
+
+        // Assert
+
+        assertEquals(expectedResult, actualResult);
+    }
+
+}
