3c936413ffa6ad4202196d0c7dc625f205e09f13
Teresa Varela Gomes <1181730@isep.ipp.pt>
2019-05-06 12:41:06 +0100

    REF #294
    DeviceList
    - coverage and bug

--- src/test/java/pt/ipp/isep/dei/project/model/device/DeviceListTest.java
+++ src/test/java/pt/ipp/isep/dei/project/model/device/DeviceListTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import pt.ipp.isep.dei.project.model.device.devicespecs.DishwasherSpec;
 import pt.ipp.isep.dei.project.model.device.devicespecs.WaterHeaterSpec;
-import pt.ipp.isep.dei.project.model.room.RoomService;
 import pt.ipp.isep.dei.project.model.device.devicespecs.FridgeSpec;
 import pt.ipp.isep.dei.project.model.device.devicespecs.WashingMachineSpec;
 import pt.ipp.isep.dei.project.model.device.log.Log;
@@ -29,6 +28,30 @@ class DeviceListTest {
         firstValidDevice = new Fridge(new FridgeSpec());
         firstValidDevice.setName("FridgeOne");
         firstValidDevice.setNominalPower(30);
+    }
+
+    @Test
+    void seeIfGetListWorks() {
+        // Arrange
+
+        List<Device> expectedResult = new ArrayList<>();
+        Device secondValidDevice = new Dishwasher(new DishwasherSpec());
+        secondValidDevice.setName("DishwasherOne");
+        secondValidDevice.setNominalPower(50);
+
+        validList.add(firstValidDevice);
+        validList.add(secondValidDevice);
+
+        expectedResult.add(firstValidDevice);
+        expectedResult.add(secondValidDevice);
+
+        // Act
+
+        List<Device> actualResult = validList.getList();
+
+        // Assert
+
+        assertEquals(expectedResult, actualResult);
     }
 
     @Test
