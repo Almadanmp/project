2db76972295fb206eddfa9ef605c2635ca1a4fc1
João Cachada <jmcs.cachada@gmail.com>
2019-05-14 16:39:43 +0100

    Refs #354
    Adds mock properly functioning web controller with basic findAll capabilities for group consultation.

--- src/main/java/pt/ipp/isep/dei/project/controllerWeb/EnergyGridSettingsWebController.java
+++ src/main/java/pt/ipp/isep/dei/project/controllerWeb/EnergyGridSettingsWebController.java
@@ -1,4 +1,21 @@
 package pt.ipp.isep.dei.project.controllerWeb;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import pt.ipp.isep.dei.project.model.geographicarea.GeographicArea;
+import pt.ipp.isep.dei.project.repository.GeographicAreaRepository;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/gridSettings")
 public class EnergyGridSettingsWebController {
+
+    @Autowired
+    private GeographicAreaRepository gridRepo;
+
+    @GetMapping(value = "/grids")
+    public @ResponseBody List<GeographicArea> test(){
+        return gridRepo.findAll();
+    }
 }
