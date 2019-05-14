db7ab42641ca905e338830b63196c446f7e80723
1181716 <1181716@isep.ipp.pt>
2019-05-14 15:17:18 +0100

    #345:GASettingsWebController added

---
+++ src/main/java/pt/ipp/isep/dei/project/controllerWeb/GASettingsWebController.java
@@ -0,0 +1,13 @@
+package pt.ipp.isep.dei.project.controllerWeb;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GASettingsWebController {
+
+
+
+
+
+
+}
