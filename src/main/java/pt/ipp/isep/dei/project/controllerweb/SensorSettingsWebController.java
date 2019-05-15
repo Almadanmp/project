739ab480d98088580968293406870f7acb16c2c4
Andre Rua <1181689@isep.ipp.pt>
2019-05-15 11:23:39 +0100

    REF #356 Merge update

--- src/main/java/pt/ipp/isep/dei/project/controllerweb/SensorSettingsWebController.java
+++ src/main/java/pt/ipp/isep/dei/project/controllerweb/SensorSettingsWebController.java
@@ -1,7 +1,6 @@
 package pt.ipp.isep.dei.project.controllerweb;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -9,12 +8,8 @@ import org.springframework.web.context.annotation.ApplicationScope;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import pt.ipp.isep.dei.project.dto.AreaSensorDTO;
 import pt.ipp.isep.dei.project.dto.GeographicAreaDTO;
-import pt.ipp.isep.dei.project.dto.mappers.AreaSensorMapper;
 import pt.ipp.isep.dei.project.dto.mappers.GeographicAreaMapper;
-import pt.ipp.isep.dei.project.model.geographicarea.AreaSensor;
-import pt.ipp.isep.dei.project.model.geographicarea.GeographicArea;
 import pt.ipp.isep.dei.project.model.geographicarea.GeographicAreaRepository;
-import pt.ipp.isep.dei.project.repository.GeographicAreaCrudeRepo;
 
 import java.net.URI;
 import java.util.List;
@@ -26,9 +21,6 @@ public class SensorSettingsWebController {
 
     @Autowired
     private GeographicAreaRepository geographicAreaRepository;
-
-    @Autowired
-    GeographicAreaCrudeRepo geographicAreaCrudRepo;
 
     // Part 1 - Geographical Areas
 
@@ -62,8 +54,8 @@ public class SensorSettingsWebController {
         return ResponseEntity.created(location).build();
     }
 
-    // us010 deactivate geo area sensor
 
+    // us010 deactivate geo area sensor
 
     //get all area sensors
     @PatchMapping(value = "/deactivate")
@@ -72,7 +64,7 @@ public class SensorSettingsWebController {
         long id = areaSensorDTO.getGeographicAreaID();
         GeographicAreaDTO geographicArea = geographicAreaRepository.getDTOById(id);
         geographicAreaRepository.updateGeoArea(GeographicAreaMapper.dtoToObject(geographicArea));
-        return new ResponseEntity <> ("body", HttpStatus.OK);
+        return new ResponseEntity<>("body", HttpStatus.OK);
     }
 }
 
