c255190c296b4004fc653f61898c387accb8a80e
Daniela Carvalho <1181701@isep.ipp.pt>
2019-05-17 09:55:23 +0100

    REF #351 adds house repository methods to get the house and return the house as a DTO

--- src/main/java/pt/ipp/isep/dei/project/model/house/HouseRepository.java
+++ src/main/java/pt/ipp/isep/dei/project/model/house/HouseRepository.java
@@ -2,6 +2,8 @@ package pt.ipp.isep.dei.project.model.house;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import pt.ipp.isep.dei.project.dto.HouseDTO;
+import pt.ipp.isep.dei.project.dto.mappers.HouseMapper;
 import pt.ipp.isep.dei.project.repository.HouseCrudeRepo;
 
 import java.util.List;
@@ -25,6 +27,25 @@ public class HouseRepository {
         List<House> houseList = houseCrudeRepo.findAll();
         House house = houseList.get(0);
         return house.getId();
+    }
+
+    /**
+     * This method goes through the House database and gets all houses on the db
+     * @return a list with all the houses found on the db
+     */
+    //TODO unit test - Daniela
+    public List<House> getHouses(){
+        return houseCrudeRepo.findAll();
+    }
+
+    /**
+     * This method goes through the House database and gets the house of the application (there is only one).
+     * @return the House of the application as a DTO
+     */
+    //TODO unit test - Daniela
+    public HouseDTO getApplicationHouse(){
+        House house =  houseCrudeRepo.findAll().get(0);
+        return HouseMapper.objectToDTO(house);
     }
 
 }
