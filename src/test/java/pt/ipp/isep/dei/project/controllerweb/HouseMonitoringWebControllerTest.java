de4e1494260dca1374db30ed477b2f90d199b850
Daniela Carvalho <1181701@isep.ipp.pt>
2019-05-21 16:10:20 +0100

    REF #373 adds tests to HouseMonitoringWebControllerTest

--- src/test/java/pt/ipp/isep/dei/project/controllerweb/HouseMonitoringWebControllerTest.java
+++ src/test/java/pt/ipp/isep/dei/project/controllerweb/HouseMonitoringWebControllerTest.java
@@ -1,24 +1,25 @@
 package pt.ipp.isep.dei.project.controllerweb;
 
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@RunWith(SpringRunner.class)
-@WebMvcTest(HouseMonitoringWebController.class)
+@ExtendWith({SpringExtension.class, MockitoExtension.class})
+@WebMvcTest
 @ContextConfiguration(classes = HibernateJpaAutoConfiguration.class)
-public class HouseMonitoringWebControllerTest {
+ class HouseMonitoringWebControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
@@ -27,7 +28,7 @@ public class HouseMonitoringWebControllerTest {
     HouseMonitoringWebController houseMonitoringWebController;
 
     @Test
-    public void getHighestAmplitudeSuccess() throws Exception {
+     void getHighestAmplitudeSuccess() throws Exception {
 
         this.mockMvc = MockMvcBuilders.standaloneSetup(houseMonitoringWebController).build();
 
@@ -41,17 +42,31 @@ public class HouseMonitoringWebControllerTest {
                 .andExpect(status().isOk());
     }
 
-//    @Test
-//    public void getHighestAmplitudeInvertedDates() throws Exception {
-//
-//        this.mockMvc = MockMvcBuilders.standaloneSetup(houseMonitoringWebController).build();
-//
-//        mockMvc.perform(get("/houseMonitoring/highestAmplitude")
-//                .contentType(MediaType.APPLICATION_JSON)
-//                .content(                        " {\n" +
-//                        "\"initialDate\": \"2019-01-01\",\n" +
-//                        "\"endDate\": \"2018-01-01\"\n" +
-//                        " }"))
-//                .andExpect(status().isBadRequest());
-//    }
+    @Test
+     void getHighestAmplitudeInvertedDates() throws Exception {
+
+        this.mockMvc = MockMvcBuilders.standaloneSetup(houseMonitoringWebController).build();
+
+        mockMvc.perform(get("/houseMonitoring/highestAmplitude")
+                .content("\n" +
+                        " {\n" +
+                        "\"initialDate\": \"2019-01-01\",\n" +
+                        "\"endDate\": \"2018-01-01\"\n" +
+                        " }"))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+     void getHighestAmplitudeNoReadingsOnInterval() throws Exception {
+
+        this.mockMvc = MockMvcBuilders.standaloneSetup(houseMonitoringWebController).build();
+
+        mockMvc.perform(get("/houseMonitoring/highestAmplitude")
+                .content("\n" +
+                        " {\n" +
+                        "\"initialDate\": \"2010-01-01\",\n" +
+                        "\"endDate\": \"2011-01-01\"\n" +
+                        " }"))
+                .andExpect(status().isBadRequest());
+    }
 }
