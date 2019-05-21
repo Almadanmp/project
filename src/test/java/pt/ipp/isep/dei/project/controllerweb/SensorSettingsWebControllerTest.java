a0dfd1dab280269095c13168f2068993a2cd3807
Dark <1181722@isep.ipp.pt>
2019-05-21 14:33:24 +0100

    REF #375 Tested removeSensor for NOT_FOUND return.

--- src/test/java/pt/ipp/isep/dei/project/controllerweb/SensorSettingsWebControllerTest.java
+++ src/test/java/pt/ipp/isep/dei/project/controllerweb/SensorSettingsWebControllerTest.java
@@ -60,7 +60,7 @@ class SensorSettingsWebControllerTest {
     }
 
     @Test
-    void seeIfRemoveAreaSensor() throws Exception {
+    void seeIfRemoveAreaSensorWorks() throws Exception {
         // Arrange
         this.mockMvc = MockMvcBuilders.standaloneSetup(sensorSettingsWebController).build();
         GeographicArea geo1 = new GeographicArea();
@@ -83,6 +83,32 @@ class SensorSettingsWebControllerTest {
 
         this.mockMvc.perform(delete("/sensorsettings/areas/1/sensors/RF12345", sensor1))
                 .andExpect(status().isOk());
+    }
+
+    @Test
+    void seeIfRemoveAreaSensorWorksForNotFound() throws Exception {
+        // Arrange
+        this.mockMvc = MockMvcBuilders.standaloneSetup(sensorSettingsWebController).build();
+        GeographicArea geo1 = new GeographicArea();
+        long id = 1;
+        geo1.setId(id);
+        geo1.setDescription("Campus do ISEP");
+        geo1.setName("ISEP");
+        geo1.setAreaTypeID("urban area");
+
+        AreaSensor sensor1 = new AreaSensor("RF1234", "Meteo station ISEP - rainfall", "rainfall", new Local(45,45,45), new Date());
+        ArrayList<AreaSensor> areaSensorArrayList = new ArrayList<>();
+        areaSensorArrayList.add(sensor1);
+        geo1.setAreaSensors(areaSensorArrayList);
+        geo1.setLocation(new Local(45,45,45));
+
+        String id2 = "RF12345";
+        Mockito.doReturn(GeographicAreaMapper.objectToDTO(geo1)).when(this.geographicAreaRepository).getDTOById(id);
+        Mockito.doReturn(false).when(this.geographicAreaRepository).removeSensorDTO(GeographicAreaMapper.objectToDTO(geo1), id2);
+        Mockito.doReturn(true).when(this.geographicAreaRepository).updateAreaDTOB(GeographicAreaMapper.objectToDTO(geo1));
+
+        this.mockMvc.perform(delete("/sensorsettings/areas/1/sensors/RF12345"))
+                .andExpect(status().isNotFound());
     }
 
 //    @Test
