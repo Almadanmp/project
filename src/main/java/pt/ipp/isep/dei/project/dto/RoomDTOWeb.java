24a31aa1a2eff44469905f86dbf9d16273a4aa0f
Teresa Varela Gomes <1181730@isep.ipp.pt>
2019-05-23 12:42:37 +0100

    REF #358
    - Altered HTTP STATUS for US105 to meet compliance
    - Altered createRoom so that is validates name and dimensions separately to meet HTTP status compliance

--- src/main/java/pt/ipp/isep/dei/project/dto/RoomDTOWeb.java
+++ src/main/java/pt/ipp/isep/dei/project/dto/RoomDTOWeb.java
@@ -5,51 +5,65 @@ import org.springframework.hateoas.ResourceSupport;
 import java.util.Objects;
 
 public class RoomDTOWeb extends ResourceSupport {
-
+    //TODO javadocs
     private String name;
     private int floor;
     private double width;
     private double length;
     private double height;
 
-    public void setWidth(double width) {
-        this.width = width;
+    public String getName() {
+        return name;
     }
 
     public void setName(String name) {
         this.name = name;
     }
 
-    public void setLength(double length) {
-        this.length = length;
-    }
-
-    public void setHeight(double height) {
-        this.height = height;
+    public int getFloor() {
+        return floor;
     }
 
     public void setFloor(int floor) {
         this.floor = floor;
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public int getFloor() {
-        return floor;
-    }
-
     public double getHeight() {
         return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
     }
 
     public double getLength() {
         return length;
     }
 
+    public void setLength(double length) {
+        this.length = length;
+    }
+
     public double getWidth() {
         return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public boolean isNameValid() {
+        return this.name != null;
+    }
+
+    public boolean areDimensionsValid() {
+        if (((Double.compare(this.width, 0.0)) == 0) || (Double.compare(this.length, 0.0) == 0)) {
+            return false;
+        }
+        if (Double.compare(this.width, 0.0) == 0) {
+            return false;
+        }
+        return true;
     }
 
     @Override
