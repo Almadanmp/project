c31c28e65e6b77348f088400557ee437d557ca7b
João Cachada <jmcs.cachada@gmail.com>
2019-05-23 16:09:38 +0100

    Refs #384
    Removes unused imports left over from previous commit.

--- src/test/java/pt/ipp/isep/dei/project/controller/controllercli/HouseMonitoringControllerTest.java
+++ src/test/java/pt/ipp/isep/dei/project/controller/controllercli/HouseMonitoringControllerTest.java
@@ -15,7 +15,6 @@ import pt.ipp.isep.dei.project.model.geographicarea.GeographicArea;
 import pt.ipp.isep.dei.project.model.geographicarea.GeographicAreaRepository;
 import pt.ipp.isep.dei.project.model.house.Address;
 import pt.ipp.isep.dei.project.model.house.House;
-import pt.ipp.isep.dei.project.model.house.HouseRepository;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -23,7 +22,6 @@ import java.util.*;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.any;
 
 /**
  * House Monitoring - Controller tests for the Command Line Interface.
