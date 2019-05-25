426ff6cf0b01368547b2cb5dd26c826f10df0a55
Andre Rua <1181689@isep.ipp.pt>
2019-05-25 17:42:08 +0100

    REF #405 Adds jpa annotation to delete child entities from database when they are no longer associated to a root in the cases where it should be happening, ie, @OneToMany relations

--- src/main/java/pt/ipp/isep/dei/project/model/energy/EnergyGrid.java
+++ src/main/java/pt/ipp/isep/dei/project/model/energy/EnergyGrid.java
@@ -28,7 +28,7 @@ public class EnergyGrid implements Metered, Root {
     @OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
     @JoinColumn(name = "energyGridId")
     private List<Room> rooms;
-    @OneToMany(cascade = CascadeType.ALL)
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval=true)
     @LazyCollection(LazyCollectionOption.FALSE) // LazyCollection fixes MultipleBag fetch Problem without having to
     // change fetch type from EAGER to LAZY
     @JoinColumn(name = "energyGridId")
