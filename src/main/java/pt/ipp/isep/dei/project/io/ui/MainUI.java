b3f049b532a35c2cd690c301a7eef0962df30c1a
Daniela <1181701@isep.ipp.pt>
2019-05-30 21:31:49 +0100

    REF #368 changes application.properties to include security related properties adds dependencies on pom.xml - security

--- src/main/java/pt/ipp/isep/dei/project/io/ui/MainUI.java
+++ src/main/java/pt/ipp/isep/dei/project/io/ui/MainUI.java
@@ -1,10 +1,16 @@
 package pt.ipp.isep.dei.project.io.ui;
 
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -53,7 +59,7 @@ public class MainUI {
     @Autowired
     private SensorSettingsUI sensorSettingsUI;
     @Autowired
-    private AutoImporter autoImporter;
+    private DataImporter dataImporter;
 
     public static void main(String[] args) {
         SpringApplication.run(MainUI.class, args);
@@ -88,7 +94,7 @@ public class MainUI {
             //LOAD PERSISTED GA DATA
 
             //MAIN CODE
-
+            servletContainer();
             Scanner enterToReturnToConsole = new Scanner(System.in);
             int option;
             while (true) {
@@ -165,7 +171,7 @@ public class MainUI {
                             activeInput = false;
                             break;
                         case 9:
-                            autoImporter.importData(house);
+                            dataImporter.importData(house);
                             returnToMenu(enterToReturnToConsole);
                             activeInput = false;
                             break;
@@ -261,5 +267,40 @@ public class MainUI {
             System.exit(0);
         }
         return gridMeteringPeriod;
+    }
+
+
+    private ServletWebServerFactory servletContainer() {
+        // Enable SSL Trafic
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
+            @Override
+            protected void postProcessContext(Context context) {
+                SecurityConstraint securityConstraint = new SecurityConstraint();
+                securityConstraint.setUserConstraint("CONFIDENTIAL");
+                SecurityCollection collection = new SecurityCollection();
+                collection.addPattern("/*");
+                securityConstraint.addCollection(collection);
+                context.addConstraint(securityConstraint);
+            }
+        };
+
+        // Add HTTP to HTTPS redirect
+        tomcat.addAdditionalTomcatConnectors(httpToHttpsRedirectConnector());
+
+        return tomcat;
+    }
+
+    /*
+    We need to redirect from HTTP to HTTPS. Without SSL, this application used
+    port 8082. With SSL it will use port 8443. So, any request for 8082 needs to be
+    redirected to HTTPS on 8443.
+     */
+    private Connector httpToHttpsRedirectConnector() {
+        Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+        connector.setScheme("http");
+        connector.setPort(8082);
+        connector.setSecure(false);
+        connector.setRedirectPort(8443);
+        return connector;
     }
 }
