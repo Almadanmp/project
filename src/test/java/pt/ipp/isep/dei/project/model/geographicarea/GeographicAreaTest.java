428a3bd2572a9835f2a111719ef10a74a2dbe602
Dark <1181722@isep.ipp.pt>
2019-05-30 15:52:38 +0100

    Codesmell

--- src/test/java/pt/ipp/isep/dei/project/model/geographicarea/GeographicAreaTest.java
+++ src/test/java/pt/ipp/isep/dei/project/model/geographicarea/GeographicAreaTest.java
@@ -56,6 +56,30 @@ class GeographicAreaTest {
     }
 
     @Test
+    void seeIfIsAreaContainedWorksFalse() {
+        // Arrange
+        Long childID = 0L;
+
+        // Act
+        boolean actualResult = secondValidArea.isAreaContained(childID);
+
+        // Assert
+        assertFalse(actualResult);
+    }
+
+//    @Test
+//    void seeIfIsAreaContainedWorksTrue() {
+//        // Arrange
+//        Long childID = 2L;
+//
+//        // Act
+//        boolean actualResult = secondValidArea.isAreaContained(childID);
+//
+//        // Assert
+//        assertTrue(actualResult);
+//    }
+
+    @Test
     void seeIfAddDaughterAreaWorks() {
         //Act
 
@@ -487,7 +511,6 @@ class GeographicAreaTest {
 
         assertFalse(actualResult);
     }
-
 
     @Test
     void seeIfGetSetDescription() {
