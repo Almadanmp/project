6b70ec3c87bd4923b7a8bb738248ea00062ca793
Andre Rua <1181689@isep.ipp.pt>
2019-06-02 20:55:11 +0100

    REF #457 #456 Adds tests to new class RoomSensorMinimalMapper

---
+++ src/test/java/pt/ipp/isep/dei/project/dto/mappers/RoomSensorMinimalMapperTest.java
@@ -0,0 +1,68 @@
+package pt.ipp.isep.dei.project.dto.mappers;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import pt.ipp.isep.dei.project.dto.ReadingDTO;
+import pt.ipp.isep.dei.project.dto.RoomSensorDTO;
+import pt.ipp.isep.dei.project.dto.RoomSensorDTOMinimal;
+import pt.ipp.isep.dei.project.model.room.RoomSensor;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RoomSensorMinimalMapperTest {
+
+    private RoomSensor roomSensor;
+    private RoomSensorDTOMinimal validRoomSensorDTO;
+
+    @BeforeEach
+    void arrangeArtifacts() {
+
+        SimpleDateFormat validSdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+        Date date = new Date();
+
+        try {
+            date = validSdf.parse("11/02/2018 10:00:00");
+
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        roomSensor = new RoomSensor("SensorDTO1", "test", "Temperature", date);
+        validRoomSensorDTO = new RoomSensorDTOMinimal();
+        validRoomSensorDTO.setActive(true);
+        validRoomSensorDTO.setId("12");
+        validRoomSensorDTO.setName("test");
+        validRoomSensorDTO.setTypeSensor("Temperature");
+        validRoomSensorDTO.setUnits("Celsius");
+
+        validRoomSensorDTO.setDateStartedFunctioning("11/02/2018 10:00:00");
+    }
+
+    @Test
+    void dtoToObject() {
+        //Act
+
+        RoomSensor actualResult = RoomSensorMinimalMapper.dtoToObject(validRoomSensorDTO);
+
+        //Assert
+
+        assertEquals(roomSensor.getName(), actualResult.getName());
+    }
+
+    @Test
+    void objectToDTO() {
+        //Act
+
+        RoomSensorDTOMinimal actualResult = RoomSensorMinimalMapper.objectToDTO(roomSensor);
+        //Assert
+
+        assertEquals(validRoomSensorDTO.getName(), actualResult.getName());
+    }
+
+}
