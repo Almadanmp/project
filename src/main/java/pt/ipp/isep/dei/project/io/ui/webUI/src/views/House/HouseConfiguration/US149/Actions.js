d5a364b3848428d14861b2f7d6c51b7e190a2120
danie <1181699@isep.ipp.pt>
2019-06-06 16:37:32 +0100

    REF #465 - US149 up and running.

--- src/main/java/pt/ipp/isep/dei/project/io/ui/webUI/src/views/House/HouseConfiguration/US149/Actions.js
+++ src/main/java/pt/ipp/isep/dei/project/io/ui/webUI/src/views/House/HouseConfiguration/US149/Actions.js
@@ -5,15 +5,13 @@ export const REMOVE_GRID_ROOM_INFO_SUCCESS = 'REMOVE_GRID_ROOM_INFO_SUCCESS';
 export const REMOVE_GRID_ROOM_INFO_FAILURE = 'REMOVE_GRID_ROOM_INFO_FAILURE';
 
 
-
 export const fetchRoomFromGrid = ({roomID, gridID}) => {
   return dispatch => {
     dispatch(fetchRoomFromGridInfo(roomID, gridID)); // antes de fazer o get, coloca o loading a true
     axios
-      .delete('http://localhost:9898/gridSettings/grids/'+gridID+'/'+roomID, //falta autorização
+      .delete('http://localhost:9898/gridSettings/grids/'+gridID, //falta autorização
         {
-          headers: {'Content-Type': 'application/json'},
-          body: {roomID}
+          data: {name: roomID}
         })
       .then(res => {
         dispatch(fetchRoomFromGridInfoSuccess(res.data)); // chegaram os resultados (dados) , loading fica a falso
