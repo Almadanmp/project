d5ab39efbf518307b68080b1c74caaf428c0091c
danie <1181699@isep.ipp.pt>
2019-06-12 16:56:48 +0100

    REF #505: US002 front-end properly done. Doesn't work due to security, given an 403 error (forbidden) and not entering  the back-end.

--- src/main/java/pt/ipp/isep/dei/project/io/ui/webUI/src/views/GeographicArea/Area.js
+++ src/main/java/pt/ipp/isep/dei/project/io/ui/webUI/src/views/GeographicArea/Area.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
 import US002 from './US002';
+import US003 from './US003';
+
 
 class Area extends Component {
   constructor(props) {
@@ -18,6 +20,7 @@ class Area extends Component {
         <h2>Welcome to the Geographic Area Menu.</h2>
         <h4>Please select the option you want to run.</h4>
         <US002/>
+        <US003/>
       </div>
     );
   }
---
+++ src/main/java/pt/ipp/isep/dei/project/io/ui/webUI/src/views/GeographicArea/US003.js
@@ -0,0 +1,33 @@
+import React, {Component} from 'react';
+import {Collapse, Button, CardBody, Card} from 'reactstrap';
+import US003Redux from "./US003/US003Redux";
+
+class US003 extends Component {
+  constructor(props) {
+    super(props);
+    this.toggle = this.toggle.bind(this);
+    this.state = {collapse: false};
+  }
+
+  toggle() {
+    this.setState(state => ({collapse: !state.collapse}));
+  }
+
+  render() {
+    return (
+      <div>
+        <Button onClick={this.toggle} style={{backgroundColor: '#FFFFFF', marginBottom: '1rem'}}>Add Geographic Area</Button>
+        <Collapse isOpen={this.state.collapse}>
+          <Card>
+            <CardBody>
+              <US003Redux/>
+            </CardBody>
+          </Card>
+        </Collapse>
+      </div>
+    );
+  }
+
+}
+
+export default US003;
