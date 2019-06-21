1f0104e01dede3b25affe9c1dc115d1f1a814a6e
Daniela <1181701@isep.ipp.pt>
2019-06-21 11:48:00 +0100

    REF #593 updates please login page with a button to login

--- src/main/java/pt/ipp/isep/dei/project/io/ui/webUI/src/views/Pages/PleaseLogin.js
+++ src/main/java/pt/ipp/isep/dei/project/io/ui/webUI/src/views/Pages/PleaseLogin.js
@@ -1,27 +1,32 @@
 import React, {Component} from 'react';
-import {Button, Col, Container, Input, InputGroup, InputGroupAddon, InputGroupText, Row} from "reactstrap";
+import {Button, Col, Container, Row} from "reactstrap";
 
 class PleaseLogin extends Component {
   constructor(props) {
     super(props);
-    this.toggle = this.toggle.bind(this);
-    this.state = { collapse: false };
+    this.onClick = this.onClick.bind(this);
+    this.state = {collapse: false};
   }
 
-  toggle() {
-    this.setState(state => ({ collapse: !state.collapse }));
+  onClick() {
+    this.props.history.replace('/login');
   }
 
   render() {
     return (
-      <div className="app flex-row align-items-center">
+      <div align="center">
         <Container>
           <Row className="justify-content-start">
             <Col md="8">
               <span className="clearfix">
                 <h4 className="pt-12">You Shall Not Pass!</h4>
                 <p className="text-muted float-left">Please log in to access this page.</p>
+
                 <img src="https://i.imgur.com/qVPAMy3.png" width="600" height="400"/>
+                <p></p>
+                <p></p>
+                <Button onClick={this.onClick} style={{backgroundColor: '#93c4c4', marginBottom: '10rem'}}
+                        class="fa fa-plus-square-o fa-lg mt-4">LOGIN</Button>
               </span>
             </Col>
           </Row>
