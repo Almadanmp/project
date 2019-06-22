1cd0100526591738b7555c9a7efffb9f9f3add48
Andre Rua <1181689@isep.ipp.pt>
2019-06-22 13:32:33 +0100

    df

--- src/main/java/pt/ipp/isep/dei/project/io/ui/webUI/src/configureStore.js
+++
@@ -1,9 +0,0 @@
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
-import combineReducers from  "./reducers.js";
-
-export default function configureStore() {
-  return createStore(
-    combineReducers,
-    applyMiddleware(thunk));
-}
--- src/main/java/pt/ipp/isep/dei/project/io/ui/webUI/src/logOut/logoutActions.js
+++
@@ -1,6 +0,0 @@
-export function logout(e) {
-  e.preventDefault()
-  localStorage.removeItem("loginToken")
-  localStorage.removeItem("user")
-  localStorage.removeItem("userRole")
-}
